13e1e6a5ec45b6711904aa2aaba2c5ee6c69af6c
nicholaspham2311 <nicholaspham2311@gmail.com>
2019-08-06 21:31:19 +0700

    update fish

---
+++ fish/config.fish
@@ -0,0 +1,10 @@
+set -g -x fish_greeting 'Hi, Master'
+alias rem='sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get autoremove -y && sudo apt autoclean'
+alias bye='sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get autoremove -y && sudo apt autoclean && sudo shutdown now'
+alias sylater='sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get autoremove -y && sudo apt autoclean && sudo reboot now'
+alias c='clear'
+alias h='history'
+alias ins='sudo apt-get install -y'
+alias uins='sudo apt-get remove -y'
+alias e='exit'
+
