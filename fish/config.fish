b8a3e06b0c99da84f9713fa1ce7d6e4d147687b9
Nicholas Pham <nicholaspham2311@gmail.com>
2019-08-20 09:55:07 +0700

    fix

--- fish/config.fish
+++ fish/config.fish
@@ -2,31 +2,32 @@
 set -g -x fish_greeting Hi, Master
 
 #alias
-alias rem='sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get autoremove -y && sudo apt autoclean'
-alias bye='sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get autoremove -y && sudo apt autoclean && sudo shutdown now'
-alias sylater='sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get autoremove -y && sudo apt autoclean && sudo reboot now'
+alias rem='sudo apt-get update ; sudo apt-get upgrade -y ; sudo apt-get autoremove -y ; sudo apt autoclean'
+alias bye='sudo apt-get update ; sudo apt-get upgrade -y ; sudo apt-get autoremove -y ; sudo apt autoclean ; sudo shutdown now'
+alias sylater='sudo apt-get update ; sudo apt-get upgrade -y ; sudo apt-get autoremove -y ; sudo apt autoclean ; sudo reboot now'
 alias c='clear'
 alias h='history'
 alias ins='sudo apt-get install -y'
 alias uins='sudo apt-get remove -y'
 alias e='exit'
 alias v='vim'
-alias ...='cd .. && cd .. && cd ..'
-alias ....='cd .. && cd .. && cd .. && cd ..'
+alias ...='cd .. ; cd .. ; cd ..'
+alias ....='cd .. ; cd .. ; cd .. ; cd ..'
+alias s='sudo'
 
 #vi mode
 fish_vi_key_bindings
 
 #git
-alias yo='git add -A && git commit -m (curl -s whatthecommit.com/index.txt)'
+alias yo='git add -A ; git commit -m (curl -s whatthecommit.com/index.txt)'
 alias push="git push"
 alias pull="git pull"
 
 #git push all repositories
-alias gpha='cd ~/git/dotfiles/fish && cp ~/.config/fish/config.fish . && echo "fish done" && cd ~/git/dotfiles/vim && cp ~/.vimrc . && echo "vim done" && cd ~/git/dotfiles && yo && push && echo "done"'
+alias gpha='cd ~/git/dotfiles/fish ; cp ~/.config/fish/config.fish . ; echo "fish done" ; cd ~/git/dotfiles/vim ; cp ~/.vimrc . ; echo "vim done" ; cd ~/git/dotfiles ; yo ; push ; echo "done"'
 
 #git pull all repositories
-alias gpla='cd ~/git/dotfiles && pull && .. && cd study && pull && .. && cd firstwebsite && pull && cd'
+alias gpla='cd ~/git/dotfiles ; pull ; .. ; cd study ; pull ; .. ; cd firstwebsite ; pull ; cd'
 
 #start a day
-alias hi='rem && gpla && gpha && c && cd && figlet Be grateful for every moment && figlet ooo && figlet I love Mater so much!'
+alias hi='rem ; gpla ; gpha ; c ; cd ; figlet Be grateful for every moment ; figlet ooo ; figlet I love Mater so much!'
