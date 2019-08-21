ce06e312f50533b77ae65529119da6eda180f136
Nicholas <nicholaspham2311@gmail.com>
2019-08-21 18:32:11 +0700

    I made leetle mistake

--- fish/config.fish
+++ fish/config.fish
@@ -24,10 +24,13 @@ alias push="git push"
 alias pull="git pull"
 
 #git push all repositories
-alias gitpushdotfile='cd ~/git/dotfiles/fish ; cp ~/.config/fish/config.fish . ; echo "fish done" ; cd ~/git/dotfiles/vim ; cp ~/.vimrc . ; echo "vim done" ; cd ~/git/dotfiles ; yo ; push ; echo "dotfile push done"'
-alias gitpushstudy='mv ~/Downloads/*.json ~/git/study/html/freecodecamp/website/Basic-HTML-and-HTML5/ ; cd ~/git/study ; yo ; push'
 
-alias gitpushall='gitpushdotfile ; gitpushstudy'
+alias gitpushdotfile='cd ~/git/dotfiles/fish ; cp ~/.config/fish/config.fish . ; echo "fish done" ; cd ~/git/dotfiles/vim ; cp ~/.vimrc . ; echo "vim done" ; cd ~/git/dotfiles ; yo ; push ; figlet dotfiles done'
+
+###bug"green": mv error -> fix:ideas "if" bug"yellow":maybe *.json not freecodecamp
+alias gitpushstudy='mv ~/Downloads/*.json ~/git/study/html/freecodecamp/website/Basic-HTML-and-HTML5/ ; cd ~/git/study ; yo ; push ; figlet study done'
+
+alias gitpushall='gitpushdotfile ; gitpushstudy ; figlet done ; cd'
 
 
 #git pull all repositories
