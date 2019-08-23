f5f45813e5eae167a298ad5786103e4098789609
Nicholas <nicholaspham2311@gmail.com>
2019-08-23 09:24:29 +0700

    Derp

--- fish/config.fish
+++ fish/config.fish
@@ -33,7 +33,4 @@ alias gitpushall='gitpushdotfile ; gitpushstudy ; figlet all-done ; cd'
 #git pull all repositories
 alias gitpullall='cd ~/git/dotfiles ; pull ; cd ~/git/study ; pull ; cd ~/git/firstwebsite ; pull ; cd'
 
-#start a day
-alias hi='gitpullall ; gitpushall ; c ; cd ; figlet Be grateful for every moment ; figlet ooo ; figlet I love Mater so much!'
-alias bye='figlet I love Master forever; gitpullall ; gitpushall ; s shutdown now'
 
