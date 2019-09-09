6d402ff6c1f67ec4b8116b48ca50dc93827ed3b0
nicholas <nicholaspham2311@gmail.com>
2019-09-09 14:14:09 +0700

    omgsosorry

--- fish/config.fish
+++ fish/config.fish
@@ -13,33 +13,36 @@ end
 
 ###########################Lazy coding 
 alias s='sudo'
+alias ..='cd ..'
 alias ...='cd .. ; cd .. ; cd ..'
 alias ....='cd .. ; cd .. ; cd .. ; cd ..'
 alias c='clear'
 alias h='history'
-alias ins='s apt-get install -y'
-alias uins='s apt-get remove -y'
+alias ins='sudo apt-get install -y'
+alias uins='sudo apt-get remove -y'
 alias e='exit'
 alias v='nvim'
-alias rem='s apt-get update ;s apt-get upgrade -y ;s apt-get autoremove -y;s apt-get autoclean'
-alias download='cd ~/Downloads'
+alias l='ls -latr'
 alias p='python3'
+alias dl='cd ~/Downloads'
+alias rem='sudo apt-get update ;sudo apt-get upgrade -y ;sudo apt-get autoremove -y;sudo apt-get autoclean ; sudo apt-get clean'
+alias j7='ssh u0_a190@192.168.1.2 -p 8022'
 
 #commit so hard, so auto write commit cool 
-alias yo='git add -A ; git commit -m (curl -s whatthecommit.com/index.txt)'
+alias autocommit='git add -A ; git commit -m (curl -s whatthecommit.com/index.txt)'
 alias push="git push"
 alias pull="git pull"
 
 #location file saved on pc, code push on github saved at other folder, that's it 
-alias ghdotfiles='cat ~/.config/nvim/init.vim > ~/.vimrc ; cp ~/.vimrc ~/git/dotfiles/vim ; cp ~/.config/nvim/init.vim ~/git/dotfiles/nvim ; cp ~/.bashrc ~/git/dotfiles/bash ; cp ~/.config/fish/config.fish ~/git/dotfiles/fish ; cd ~/git/dotfiles ; yo ; push'
-
-#auto lazy coding 
-alias ghfirstwebsite='cd ~/git/firstwebsite/ ; yo ; push'
+alias gitpushdotfiles='cat ~/.config/nvim/init.vim > ~/.vimrc ; cp ~/.vimrc ~/git/dotfiles/vim ; cp ~/.config/nvim/init.vim ~/git/dotfiles/nvim ; cp ~/.bashrc ~/git/dotfiles/bash ; cp ~/.config/fish/config.fish ~/git/dotfiles/fish/ ; cd ~/git/dotfiles ; autocommit ; push'
+alias gitpushfirstwebsite='cd ~/git/firstwebsite/ ; autocommit ; push'
 
 #learning from freecodecamp, when done task, archive file .json , download it
-#alias ghstudy='mv ~/Downloads/*.json ~/git/study/html/freecodecamp/website/Basic-HTML-and-HTML5/ ; cd ~/git/study ; yo ; push'
-alias ghstudy='cd ~/git/study ; yo ; push'
+#alias gitpushstudy='mv ~/Downloads/*.json ~/git/study/html/freecodecamp/website/Basic-HTML-and-HTML5/ ; cd ~/git/study ; autocommit ; push'
+alias gitpushstudy='cd ~/git/study ; autocommit ; push'
 
 #cowsay make fun
-alias gha='ghdotfiles ; ghstudy ; ghfirstwebsite ; fortune | cowsay; cd'
-alias gla='cd ~/git/dotfiles ; pull ; cd ~/git/study ; pull ; cd ~/git/firstwebsite ; pull ; cd ; cowsay git pull all done'
+alias gitpushall='gitpushdotfiles ; gitpushstudy ; gitpushfirstwebsite ; fortune | cowsay; cd'
+alias gitpullall='cd ~/git/dotfiles ; pull ; cd ~/git/study ; pull ; cd ~/git/firstwebsite ; pull ; cd ; cowsay git pull all done'
+
+alias hi='cowsay Hi ; gitpullall ; gitpushall ; rem'
