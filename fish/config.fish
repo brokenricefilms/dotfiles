80303831a6ad631f9d651908d2f4524334b17025
nicholas <nicholaspham2311@gmail.com>
2019-10-09 16:30:19 +0700

    I forgot to commit... So here you go.

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,6 @@
 #!/usr/bin/fish
-set -g -x fish_greeting Hi, Nicholas 
 
+set -g -x fish_greeting Hi, Nicholas 
 fish_vi_key_bindings
 
 #able sudo!! for fish
@@ -28,7 +28,6 @@ alias p='python3'
 alias rem='sudo apt-get update ;sudo apt-get upgrade -y ;sudo apt-get autoremove -y;sudo apt-get autoclean ; sudo apt-get clean'
 alias rbn='sudo reboot now'
 alias sdn='sudo shutdown now'
-###lazy code###
 
 ###git###
 #commit so hard, so auto write commit cool 
@@ -48,7 +47,6 @@ alias glstudy='cd ~/git/study ; pull'
 alias glfirstwebsite='cd ~/git/firstwebsite ; pull'
 alias glpomodoro='cd ~/git/pomodoro ; pull'
 alias gla='gldotfiles ; glstudy ; glfirstwebsite ; glpomodoro ; cd ; cowthink -s "git pull all done"'
-###git###
 
 ###firefox###
 alias fi='firefox -new-firefox'
@@ -72,7 +70,6 @@ alias fi_codeaholicguy='firefox "https://codeaholicguy.com/"'
 alias fi_thai='firefox "https://vnhacker.blogspot.com/"'
 alias fi_ml='firefox "https://l4w.io/"'
 alias fi_blogs='firefox_toidicodedao ; firefox_thefullsnack ; firefox_codeaholicguy ; firefox_thai ; firefox_ml'
-###firefox###
 
 ###chrome###
 alias ch='google-chrome'
@@ -98,9 +95,7 @@ alias ch_codeaholicguy='ch "https://codeaholicguy.com/"'
 alias ch_thai='ch "https://vnhacker.blogspot.com/"'
 alias ch_ml='ch "https://l4w.io/"'
 alias ch_blogs='ch_toidicodedao ; ch_thefullsnack ; ch_codeaholicguy ; ch_thai ; ch_ml'
-###chrome###
 
 ###mode###
 alias work='fortune | cowsay ; ch_pomodoro ; ch_music_chill_japan ; gla ; gha'
 alias hola='fortune | cowsay ; ch_fun ; ch_youtube_subsriptions ; ch_blogs ; ch_fb ; ch_music_rapvn ; rem ; gla ; gha'
-###mode###
