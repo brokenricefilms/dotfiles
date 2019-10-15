00c5e9fd76b668a3c55ba5d9f1908c4fc93034b9
nicholas <nicholaspham2311@gmail.com>
2019-10-15 10:04:41 +0700

    Fixing Rafael's bug.

--- fish/config.fish
+++ fish/config.fish
@@ -101,6 +101,6 @@ alias ch_blogs='ch_toidicodedao ; ch_thefullsnack ; ch_codeaholicguy ; ch_thai ;
 ###chrome###
 
 ###mode###
-alias work='fortune | cowsay ; ch_music_chill_japan ; gla ; gha'
-alias hola='fortune | cowsay ; ch_fun ; ch_youtube_subsriptions ; ch_blogs ; ch_fb ; ch_music_rapvn ; rem ; gla ; gha'
+alias work='fortune | cowsay ; ch_music_chill_japan ; ch_pomodoro ; gla ; gha'
+alias hola='fortune | cowsay ; ch_youtube_subsriptions ; ch_blogs ; ch_fb ; rem ; gla ; gha'
 ###mode###
