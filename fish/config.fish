4ee0f05b3865544aff566d2bff2f16222970ee8f
nicholas <nicholaspham2311@gmail.com>
2020-01-09 17:54:02 +0700

    Added some NullPointerExceptions - Happy easter, you bastards! :D

--- fish/config.fish
+++ fish/config.fish
@@ -60,7 +60,7 @@ alias pull="git pull"
 alias cm='git commit -m'
 alias ok='yo ; push'
 
-alias ghdotfiles='cp ~/.gitconfig  ~/git/dotfiles/git/ ; cp ~/.tmux.conf ~/git/dotfiles/tmux ; cp ~/.config/nvim/init.vim ~/git/dotfiles/nvim ; cp ~/.config/fish/config.fish ~/git/dotfiles/fish/ ; cp ~/.ssh/config ~/git/dotfiles/ssh/ ; cp ~/.config/ranger/* ~/git/dotfiles/ranger ; cd ~/git/dotfiles ; yo ; push ; cd -'
+alias ghdotfiles='cp ~/.selected_editor ~/git/dotfiles ; cp ~/.gitconfig  ~/git/dotfiles/git/ ; cp ~/.tmux.conf ~/git/dotfiles/tmux ; cp ~/.config/nvim/init.vim ~/git/dotfiles/nvim ; cp ~/.config/fish/config.fish ~/git/dotfiles/fish/ ; cp ~/.ssh/config ~/git/dotfiles/ssh/ ; cp ~/.config/ranger/* ~/git/dotfiles/ranger ; cd ~/git/dotfiles ; yo ; push ; cd -'
 alias ghdotfiles-termux='cd ~/git/dotfiles-termux ; yo ; push ; cd -'
 alias ghlazyscript='cd ~/git/lazyscript ; yo ; push ; cd -'
 alias ghstudy='cd ~/git/study ; yo ; push ; cd -'
