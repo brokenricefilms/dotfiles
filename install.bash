f697f2b64e2fb438d886e391854aa1f7ec0f864f
nicholas <nicholaspham2311@gmail.com>
2020-01-12 14:36:10 +0700

    Made it to compile...

--- install.bash
+++ install.bash
@@ -13,3 +13,10 @@ cp ~/dotfiles/fish/config.fish ~/.config/fish/
 cp ~/dotfiles/fish/fish_prompt.fish ~/.config/fish/functions
 cp ~/dotfiles/ranger/* ~/.config/ranger
 "
+
+git clone https://github.com/nicholaspham2311/dotfiles.git
+cp ~/dotfiles/nvim/init.vim ~/.config/nvim/
+cp ~/dotfiles/tmux/.tmux.conf ~/
+cp ~/dotfiles/fish/config.fish ~/.config/fish/
+cp ~/dotfiles/fish/fish_prompt.fish ~/.config/fish/functions
+cp ~/dotfiles/ranger/* ~/.config/ranger
