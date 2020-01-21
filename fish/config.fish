74d26f0a9608929badcfced264d16beba4c0dbd1
nicholas <nicholaspham2311@gmail.com>
2020-01-21 22:02:58 +0700

    Update .gitignore

--- fish/config.fish
+++ fish/config.fish
@@ -68,15 +68,13 @@ alias cm='git commit -m'
 alias ok='yo ; push'
 
 alias ghdotfiles='cp ~/.selected_editor ~/git/dotfiles ; cp ~/.gitconfig  ~/git/dotfiles/git/ ; cp ~/.tmux.conf ~/git/dotfiles/tmux ; cp ~/.config/nvim/init.vim ~/git/dotfiles/nvim ; cp ~/.config/fish/config.fish ~/git/dotfiles/fish/ ; cp ~/.config/fish/functions/fish_prompt.fish ~/git/dotfiles/fish/ ; cp ~/.ssh/config ~/git/dotfiles/ssh/ ; cp ~/.config/ranger/* ~/git/dotfiles/ranger ; cd ~/git/dotfiles ; yo ; push ; cd -'
-alias ghdotfiles-termux='cd ~/git/dotfiles-termux ; yo ; push ; cd -'
 alias ghlazyscript='cd ~/git/lazyscript ; yo ; push ; cd -'
-alias gha='ghlazyscript ; ghdotfiles ; ghdotfiles-termux'
+alias gha='ghlazyscript ; ghdotfiles'
 
 alias glsherlock='cd ~/tools/hacking/sherlock/ ; pull ; cd -'
 alias gldotfiles='cd ~/git/dotfiles ; pull ; cd -'
 alias gllazyscript='cd ~/git/lazyscript ; pull ; cd -'
-alias gldotfiles-termux='cd ~/git/dotfiles-termux ; pull ; cd -'
-alias gla='gldotfiles-termux ; glsherlock ; gldotfiles ; gllazyscript'
+alias gla='glsherlock ; gldotfiles ; gllazyscript'
 
 #browser
 alias ch='google-chrome'
