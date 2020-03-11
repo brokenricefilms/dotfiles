dbbd8a603bc96b0199cc5d663214f90dcc2cf680
nicholas <nicholaspham2311@gmail.com>
2020-03-11 12:23:17 +0700

    TODO: Fix later

--- fish/config.fish
+++ fish/config.fish
@@ -128,7 +128,26 @@ alias ghlinux_setup='cd ~/git/linux_setup ; ok ; cd -'
 alias ghvimium_theme='cd ~/git/vimium_theme ; ok ; cd -'
 alias ghFreeCodeCampProject='cd ~/git/FreeCodeCampProject ; ok ; cd -'
 alias ghok='cd ~/git/ok ; ok ; cd -'
-alias gha='cowsay "git push lazyscript" ; ghlazyscript ; cowsay "git push dotfiles" ; ghdotfiles ; cowsay "git push linux_setup" ; ghlinux_setup ; cowsay "git push vimium_theme" ; ghvimium_theme ; cowsay "git push FreeCodeCampProject" ; ghFreeCodeCampProject ; cowsay "git push ok" ; ghok'
+# alias gha='cowsay "git push lazyscript" ; ghlazyscript ; cowsay "git push dotfiles" ; ghdotfiles ; cowsay "git push linux_setup" ; ghlinux_setup ; cowsay "git push vimium_theme" ; ghvimium_theme ; cowsay "git push FreeCodeCampProject" ; ghFreeCodeCampProject ; cowsay "git push ok" ; ghok'
+function gha --description "git push all project"
+	cowsay "git push lazyscript" 
+	ghlazyscript 
+
+	cowsay "git push dotfiles" 
+	ghdotfiles 
+
+	cowsay "git push linux_setup" 
+	ghlinux_setup 
+
+	cowsay "git push vimium_theme" 
+	ghvimium_theme 
+
+	cowsay "git push FreeCodeCampProject" 
+	ghFreeCodeCampProject 
+
+	cowsay "git push ok" 
+	ghok
+end
 
 alias glsherlock='cd ~/tools/hacking/sherlock/ ; pull ; cd -'
 alias gldotfiles='cd ~/git/dotfiles ; pull ; cd -'
