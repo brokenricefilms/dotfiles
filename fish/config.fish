2d50f6140d260e8bf4d7dc276b3410f1552a89fc
thuan <thuanpham2311@gmail.com>
2020-06-05 17:05:13 +0700

    Florian broke the regex, lame

--- fish/config.fish
+++ fish/config.fish
@@ -251,7 +251,7 @@ end
 # browser
 alias browser='brave-browser'
 # alias browser='firefox'
-alias github='browser "https://github.com/nicholaspham2311"'
+alias github='browser "https://github.com/thuanpham2311"'
 
 alias browser_youtube_subsriptions='browser "https://www.youtube.com/feed/subscriptions"'
 alias browser_fb='browser https://facebook.com'
