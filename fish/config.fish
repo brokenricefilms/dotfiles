06ea77b83dfb6ecc41ce11a5fb897e3d5f92957d
thuan <phamtanthuan2311@gmail.com>
2020-07-05 12:00:35 +0700

    mergeconflix is the new hottest Gaul on the block

--- fish/config.fish
+++ fish/config.fish
@@ -210,7 +210,6 @@ alias ghdataLab='cd ~/git/dataLab ; okp ; cd -'
 alias ghwindowsSetup='cd ~/git/windowsSetup ; okp ; cd -'
 alias ghtermuxSetup='cd ~/git/termuxSetup ; okp ; cd -'
 alias ghimg='cd ~/git/img ; okp ; cd -'
-alias ghnote='cd ~/git/note ; ok ; cd -'
 
 function gha --description "git push all project"
 	cowsay "git push lazyscript"
@@ -243,8 +242,6 @@ function gha --description "git push all project"
 	cowsay "git push img" 
     ghimg
 
-	cowsay "git push note" 
-    ghnote
 	cowsay "D O N E"
 end
 
@@ -258,7 +255,6 @@ alias gldataLab='cd ~/git/dataLab ; pull ; cd -'
 alias glwindowsSetup='cd ~/git/windowsSetup ;  pull ; cd -'
 alias gltermuxSetup='cd ~/git/termuxSetup ;  pull ; cd -'
 alias glimg='cd ~/git/img ; pull ; cd -'
-alias glnote='cd ~/git/note ; pull ; cd -'
 
 function gla --description "git pull all project"
 	cowsay "git pull lazyscript" 
@@ -291,8 +287,6 @@ function gla --description "git pull all project"
 	cowsay "git pull img" 
     glimg
 
-	cowsay "git pull note" 
-    glnote
 end
 
 # browser
