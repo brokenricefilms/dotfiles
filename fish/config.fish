c25895654838067990073ac2a5219530c4e13e53
thuan <phamtanthuan2311@gmail.com>
2020-07-13 22:00:40 +0700

    this is why docs are important

--- fish/config.fish
+++ fish/config.fish
@@ -246,7 +246,6 @@ alias ghdataLab='cd ~/git/dataLab ; okp ; cd -'
 alias ghwindowsSetup='cd ~/git/windowsSetup ; okp ; cd -'
 alias ghtermuxSetup='cd ~/git/termuxSetup ; okp ; cd -'
 alias ghimg='cd ~/git/img ; okp ; cd -'
-alias ghnote='cd ~/git/note ; ok ; cd -'
 
 function gha --description "git push all project"
 	cowsay "git push lazyscript"
@@ -279,9 +278,6 @@ function gha --description "git push all project"
 	cowsay "git push img" 
     ghimg
 
-	cowsay "git push note" 
-    ghnote
-
 	cowsay "D O N E"
 end
 
@@ -295,7 +291,6 @@ alias gldataLab='cd ~/git/dataLab ; pull ; cd -'
 alias glwindowsSetup='cd ~/git/windowsSetup ;  pull ; cd -'
 alias gltermuxSetup='cd ~/git/termuxSetup ;  pull ; cd -'
 alias glimg='cd ~/git/img ; pull ; cd -'
-alias glnote='cd ~/git/note ; pull ; cd -'
 
 function gla --description "git pull all project"
 	cowsay "git pull lazyscript" 
@@ -328,8 +323,6 @@ function gla --description "git pull all project"
 	cowsay "git pull img" 
     glimg
 
-	cowsay "git pull note" 
-    glnote
 end
 
 # browser
