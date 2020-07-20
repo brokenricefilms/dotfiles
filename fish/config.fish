283b9b0c3210d48c7ad614396d9ed0d48b266818
thuan <phamtanthuan2311@gmail.com>
2020-07-20 15:00:44 +0700

    DNS_PROBE_FINISHED_NXDOMAIN

--- fish/config.fish
+++ fish/config.fish
@@ -391,7 +391,7 @@ end
 
 # mode
 alias hi='tv; browser_daily'
-alias rem='sudo dnf update -y ; sudo dnf autoremove'
+alias rem='sudo dnf update -y ; sudo dnf autoremove -y'
 # alias rem='sudo apt update ; sudo apt upgrade -y ; sudo apt autoremove -y ; sudo apt autoclean'
 
 function data --description "setup for data research"
