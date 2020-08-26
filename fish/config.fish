01c5e301351be74f244933b70bd292e404328268
thuan <phamtanthuan2311@gmail.com>
2020-08-26 15:00:51 +0700

    Herp derp I left the debug in there and forgot to reset errors.

--- fish/config.fish
+++ fish/config.fish
@@ -89,6 +89,7 @@ alias l='clear ; exa -al --color=always --group-directories-first'
 alias ls='exa -al --color=always --group-directories-first' # my preferred listing
 alias la='exa -a --color=always --group-directories-first'  # all files and dirs
 alias ll='exa -l --color=always --group-directories-first'  # long format
+# tree add more color
 alias lt='exa -aT --color=always --group-directories-first' # tree listing
 alias l.='exa -a | egrep "^\."'
 alias fd='fdfind'
@@ -454,7 +455,6 @@ function rem
     sudo apt autoclean
     tldr --update
     sudo npm install -g npm
-    nvim -c "PlugUpdate | qa"
     clear
 end
 
