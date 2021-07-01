49cb82a6a85ef00e266061508e53b4055304babe
thuan <phamtanthuan2311@gmail.com>
2021-07-01 18:40:33 +0700

    feat(surfingkeys): new world

---
+++ surfingkeys.js
@@ -0,0 +1,2 @@
+Hints.characters = 'asdghklqwertyuiopzxcvbnmfj;';
+Hints.style('font-size: 13px; font-family: saurce code pro; border: 0px; color:#444444; background: initial; background-color: #eeeeee;');
