e1fecb65ba63d966720d8ff779e121778018c686
thuan <phamtanthuan2311@gmail.com>
2021-07-29 19:29:34 +0700

    fix(surfingkeys): visual select text suck

--- surfingkeys.js
+++ surfingkeys.js
@@ -1,2 +1,6 @@
 Hints.characters = 'asdghklqwertyuiopzxcvbnmfj;';
-Hints.style('font-size: 14px; font-family: roboto; border: 0px; color:#444444; background: initial; background-color: #eeeeee;');
+
+const hintsCss = 'font-size: 14px; font-family: roboto; border: 0px; color:#444444; background: initial; background-color: #eeeeee;'
+
+Hints.style(hintsCss)
+Hints.style(hintsCss, 'text')
