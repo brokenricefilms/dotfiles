dd78ae4e8706ea95742783cdbaf9b30717e8f630
thuan <phamtanthuan2311@gmail.com>
2021-09-06 19:26:43 +0700

    feat(surfingkeys): zenbones theme

--- surfingkeys.js
+++ surfingkeys.js
@@ -1,6 +1,6 @@
 Hints.characters = 'asdghklqwertyuiopzxcvbnmfj;';
 
-const hintsCss = 'font-size: 14px; font-family: roboto; border: 0px; color:#444444; background: initial; background-color: #eeeeee;'
+const hintsCss = 'font-size: 14px; font-family: roboto; border: 0px; color:#2C363C; background: initial; background-color: #F0EDEC;'
 
 Hints.style(hintsCss)
 Hints.style(hintsCss, 'text')
