d02e2dfb25c2a9f692794968a131032d0b8a0394
Thuan Pham <phamtanthuan2311@gmail.com>
2021-09-17 18:21:15 +0700

    🌟feature(nvim): autopair `enter` in `{}` to tab in new line

--- nvim/after/plugin/autopairs.lua
+++ nvim/after/plugin/autopairs.lua
@@ -4,3 +4,20 @@ if (not status) then return end
 autopairs.setup({
   disable_filetype = { "TelescopePrompt" , "vimwiki" },
 })
+
+local remap = vim.api.nvim_set_keymap
+local npairs = require('nvim-autopairs')
+
+-- skip it, if you use another global object
+_G.MUtils= {}
+
+MUtils.completion_confirm=function()
+  if vim.fn.pumvisible() ~= 0  then
+      return npairs.esc("<cr>")
+  else
+    return npairs.autopairs_cr()
+  end
+end
+
+
+remap('i' , '<CR>','v:lua.MUtils.completion_confirm()', {expr = true , noremap = true})
