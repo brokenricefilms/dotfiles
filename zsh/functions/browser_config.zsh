3e3ac5482f37d6f7da1124e336e6cbc30b68a5c9
Thuan Pham <phamtanthuan2311@gmail.com>
2021-09-19 21:24:39 +0700

    🌟feature(browser): open search from cli in new window

--- zsh/functions/browser_config.zsh
+++ zsh/functions/browser_config.zsh
@@ -1,7 +1,8 @@
 #! /usr/bin/env zsh
 
 alias browser='xdg-open'
-alias github='browser --new-window "https://github.com/thuanpham2311"'
+alias browser_new_tab='brave-browser --new-window'
+alias github='brave-browser --new-window "https://github.com/thuanpham2311"'
 alias browser_fb='browser https://facebook.com'
 alias browser_mes='browser "https://www.facebook.com/messages/t/thuanpham2311"'
 alias browser_mail0='browser "https://mail.google.com/mail/u/0/#inbox"'
@@ -13,19 +14,19 @@ alias browser_youtube_subsriptions='browser "https://www.youtube.com/feed/subscr
 alias browser_feedly='browser "https://feedly.com/i/collection/content/user/0d89157e-ce7d-4b2c-a2d6-98826f1e5089/category/global.all"'
 alias browser_google_podcast_subscriptions='browser "https://podcasts.google.com/subscriptions"'
 
-:t() { browser "https://translate.google.com/?source=osdd#auto|auto|$argv" }
+:t() { browser_new_tab "https://translate.google.com/?source=osdd#auto|auto|$argv" }
 
-:y() { browser "https://www.youtube.com/results?search_query=$argv" }
+:y() { browser_new_tab "https://www.youtube.com/results?search_query=$argv" }
 
-:d() { browser "https://duckduckgo.com/?q=$argv" }
+:d() { browser_new_tab "https://duckduckgo.com/?q=$argv" }
 
-:gm() { browser "https://www.google.com/maps?q=$argv" }
+:gm() { browser_new_tab "https://www.google.com/maps?q=$argv" }
 
-:g() { browser "https://www.google.com/search?q=$argv" }
+:g() { browser_new_tab "https://www.google.com/search?q=$argv" }
 
-:gh() { browser "https://github.com/search?q=$argv" }
+:gh() { browser_new_tab "https://github.com/search?q=$argv" }
 
-:f() { browser "https://www.facebook.com/search?q=$argv" }
+:f() { browser_new_tab "https://www.facebook.com/search?q=$argv" }
 
 function browser_daily () {
     cowsay "GET.SHIT.DONE"
@@ -39,4 +40,3 @@ function browser_daily () {
     browser_stu2
     firefox "facebook.com"
 }
-
