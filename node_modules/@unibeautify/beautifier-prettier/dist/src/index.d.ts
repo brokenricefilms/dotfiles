d0a14365b5baf93f719e9de5bf3aabda20544feb
Thuan Pham <phamtanthuan2311@gmail.com>
2021-10-15 13:55:13 +0700

    🌟feature(zsh): kill all unname tmux session (close #28)

--- node_modules/@unibeautify/beautifier-prettier/dist/src/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { Beautifier } from "unibeautify";
-export declare const beautifier: Beautifier;
-export default beautifier;
--- node_modules/unibeautify/dist/src/DependencyManager/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from "./Dependency";
-export * from "./DependencyManager";
-export * from "./DependencyFactory";
-export * from "./NodeDependency";
-export * from "./ExecutableDependency";
-export * from "./Version";
