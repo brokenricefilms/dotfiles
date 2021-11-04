ab6661b7af35925715d80e9185f9a385a0ae0ec2
Thuan Pham <phamtanthuan2311@gmail.com>
2021-11-04 22:57:14 +0700

    🌟feature(zsh): man fzf

--- zsh/functions/fzfConfig.zsh
+++ zsh/functions/fzfConfig.zsh
@@ -48,3 +48,7 @@ function O() {
 function o() {
     xdg-open "$(fzfDown --preview 'bat --style=numbers --color=always --line-range :500 {}')"
 }
+
+fman() {
+    man -k . | fzf -q "$1" --prompt='man> '  --preview $'echo {} | tr -d \'()\' | awk \'{printf "%s ", $2} {print $1}\' | xargs -r man | col -bx | bat -l man -p --color always' | tr -d '()' | awk '{printf "%s ", $2} {print $1}' | xargs -r man
+}
