ecca12ca1b537540f60824235e165a31096a9d4c
Thuan Pham <phamtanthuan2311@gmail.com>
2021-11-12 20:46:49 +0700

    🌟feature(zsh): vpn command connect to japan, korea, stop vpn

--- zsh/functions/vpn.zsh
+++ zsh/functions/vpn.zsh
@@ -1,2 +1,3 @@
-alias vpnKorea="sudo openvpn --config ~/dotfiles/vpn/korea.ovpn &"
-alias vpnJapan="sudo openvpn --config ~/dotfiles/vpn/japan.ovpn &"
+alias vpnKorea="sudo openvpn --config ~/dotfiles/vpn/korea.ovpn > /dev/null 2>&1 &"
+alias vpnJapan="sudo openvpn --config ~/dotfiles/vpn/japan.ovpn > /dev/null 2>&1 &"
+alias vpnStop="sudo killall openvpn"
