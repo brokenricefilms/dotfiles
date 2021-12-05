f5c1db1da914d89e1f5208e2483ec4a624419506
Thuan Pham <phamtanthuan2311@gmail.com>
2021-12-05 14:29:28 +0700

    🌟feature(nvim): switch to commment.nvim

---
+++ nvim/after/plugin/comment_nvim.lua
@@ -0,0 +1,22 @@
+require('Comment').setup {
+    padding = true,
+    sticky = true,
+    ignore = nil,
+
+    toggler = {
+        line = 'mm',
+    },
+
+    opleader = {
+        block = 'm',
+    },
+
+    mappings = {
+        basic = true,
+        extra = true,
+        extended = false,
+    },
+
+    pre_hook = nil,
+    post_hook = nil,
+}
