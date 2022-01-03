08bb1129676d7ef82b4c1e51980e2c59b798d21d
Thuan Pham <phamtanthuan2311@gmail.com>
2022-01-03 21:05:46 +0700

    🌟feature(ts auto tag): update on insert

--- nvim/after/plugin/ts-autotag.lua
+++ nvim/after/plugin/ts-autotag.lua
@@ -8,3 +8,15 @@ local skip_tags = {
   'area', 'base', 'br', 'col', 'command', 'embed', 'hr', 'img', 'slot',
   'input', 'keygen', 'link', 'meta', 'param', 'source', 'track', 'wbr','menuitem'
 }
+
+vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
+    vim.lsp.diagnostic.on_publish_diagnostics,
+    {
+        underline = true,
+        virtual_text = {
+            spacing = 5,
+            severity_limit = 'Warning',
+        },
+        update_in_insert = true,
+    }
+)
