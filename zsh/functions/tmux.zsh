650ab1aeaf6a36b7b4708a87ddb27ccb166c048c
Thuan Pham <phamtanthuan2311@gmail.com>
2022-02-12 09:30:32 +0700

    🎨refactor(zsh/functions/tmux.zsh): restructure

---
+++ zsh/functions/tmux.zsh
@@ -0,0 +1,28 @@
+alias ide='tmux split-window -v -p 20 ; tmux split-window -h -p 50 ; tmux last-pane ; nvim'
+# alias ide='tmux split-window -v -p 8 ; tmux last-pane ; nvim'
+# alias ide='tmux split-window -h -p 30 ; tmux split-window -v -p 75 ; tmux last-pane ; nvim'
+
+function killAllUnnameTmuxSession() {
+	echo "kill all unname tmux session"
+	cd /tmp/
+	tmux ls | awk '{print $1}' | grep -o '[0-9]\+' >/tmp/killAllUnnameTmuxSessionOutput.sh
+	sed -i 's/^/tmux kill-session -t /' killAllUnnameTmuxSessionOutput.sh
+	chmod +x killAllUnnameTmuxSessionOutput.sh
+	./killAllUnnameTmuxSessionOutput.sh
+	cd -
+	tmux ls
+}
+
+function killAllUnnameTmuxSessionNoMessage() {
+	cd /tmp/
+	tmux ls | awk '{print $1}' | grep -o '[0-9]\+' >/tmp/killAllUnnameTmuxSessionOutput.sh
+	sed -i 's/^/tmux kill-session -t /' killAllUnnameTmuxSessionOutput.sh
+	chmod +x killAllUnnameTmuxSessionOutput.sh
+	./killAllUnnameTmuxSessionOutput.sh
+	cd -
+}
+
+alias clear='killAllUnnameTmuxSessionNoMessage ; clear'
+alias cler='killAllUnnameTmuxSessionNoMessage ; clear'
+alias clea='killAllUnnameTmuxSessionNoMessage ; clear'
+alias cl='killAllUnnameTmuxSessionNoMessage ; clear'
