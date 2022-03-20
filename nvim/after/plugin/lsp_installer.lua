6ff7715a70eba868d6c229afb557b1b1dab3ef83
Thuan Pham <phamtanthuan2311@gmail.com>
2022-03-20 12:46:59 +0700

    🐞fix(nvim lsp): unuse global vim

--- nvim/after/plugin/lsp_installer.lua
+++ nvim/after/plugin/lsp_installer.lua
@@ -2,5 +2,14 @@ local lsp_installer = require("nvim-lsp-installer")
 
 lsp_installer.on_server_ready(function(server)
 	local opts = {}
+
+	if server.name == "sumneko_lua" then
+		opts.settings = {
+			Lua = {
+				diagnostics = { globals = { "vim" } },
+			},
+		}
+	end
+
 	server:setup(opts)
 end)
