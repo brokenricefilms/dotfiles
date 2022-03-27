91493379ef7cf7ef5feac9ec8b185eb0ba2ea0d9
Thuan Pham <phamtanthuan2311@gmail.com>
2022-03-27 09:13:10 +0700

    🎨refactor(dns.zsh): using cowsay with if

--- zsh/functions/dns.zsh
+++ zsh/functions/dns.zsh
@@ -1,12 +1,14 @@
 function updateDns() {
   sudo curl https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling/hosts --output /etc/hosts
-  echo "sometimes if you not lazy try to clear host cache: chrome://net-internals/#dns"
+  if hash cowsay 2>/dev/null; then
+    cowsay "sometimes if you not lazy try to clear host cache: chrome://net-internals/#dns"
+  else
+    echo "sometimes if you not lazy try to clear host cache: chrome://net-internals/#dns"
+  fi
   sudo systemctl restart NetworkManager.service
 }
 
 function updateDnsNoMessage() {
   sudo curl https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling/hosts --output /etc/hosts
-  echo "sometimes if you not lazy try to clear host cache: chrome://net-internals/#dns"
   sudo systemctl restart NetworkManager.service
 }
-
