2c89bce90e90cfb263deb3f6ece7a4b6931cd1a1
Thuan Pham <phamtanthuan2311@gmail.com>
2022-03-30 15:22:18 +0700

    🎨refactor(commandStartupShell): cowsay for fun

--- zsh/functions/commandStartupShell.zsh
+++ zsh/functions/commandStartupShell.zsh
@@ -1,16 +1,32 @@
 function gitDir() {
-	isInGitRepo || return
-	echo ""
-	echo "\033[0;32m❯\033[0m git status"
-	echo ""
-	git status -sb
+  isInGitRepo || return
+
+  echo ""
+
+  if hash cowsay 2>/dev/null; then
+    cowsayRandom "git status"
+  else
+    echo "\033[0;32m❯\033[0m git status"
+  fi
+
+  echo ""
+
+  git status -sb
 }
 
 function mainThing() {
-	echo ""
-	echo "\033[0;32m❯\033[0m you in $(pwd)"
-	echo ""
-	la
+
+  echo ""
+
+  if hash cowsay 2>/dev/null; then
+    cowsayRandom "you in $(pwd)"
+  else
+    echo "\033[0;32m❯\033[0m you in $(pwd)"
+  fi
+
+  echo ""
+
+  la
 }
 
 mainThing
