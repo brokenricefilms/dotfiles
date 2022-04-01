ef83a7a85e5e71d0ee338309f8e0e442aa247d7a
Thuan Pham <phamtanthuan2311@gmail.com>
2022-04-01 18:20:27 +0700

    🎨refactor(zsh): set alias, functions stuff base on linux distro (close

--- zsh/functions/debian.zsh
+++
@@ -1,3 +0,0 @@
-# alias ins='sudo apt install -y'
-# alias inss='apt search'
-# alias uins='sudo apt remove -y'
--- zsh/functions/fedora.zsh
+++
@@ -1,3 +0,0 @@
-# alias ins='sudo dnf -y install'
-# alias inss='dnf search'
-# alias uins='sudo dnf -y remove'
