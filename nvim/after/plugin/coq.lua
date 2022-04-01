c4d2c2f51be510b7313013ec38e239bbab0d1bde
Thuan Pham <phamtanthuan2311@gmail.com>
2022-04-01 11:20:07 +0700

    🌟feature(nvim): coq snippet first

---
+++ nvim/after/plugin/coq.lua
@@ -0,0 +1,29 @@
+vim.g.coq_settings = {
+	keymap = {
+		recommended = false,
+		jump_to_mark = "<c-e>",
+		manual_complete = ";;",
+	},
+	clients = {
+		buffers = {
+			enabled = true,
+			weight_adjust = -1.9,
+		},
+		tree_sitter = {
+			enabled = true,
+			weight_adjust = -1.5,
+		},
+		lsp = {
+			enabled = true,
+			weight_adjust = 1.5,
+		},
+		snippets = {
+			enabled = true,
+			weight_adjust = 1.9,
+		},
+	},
+}
+
+require("coq_3p")({
+	{ src = "copilot", short_name = "COP", accept_key = "<c-f>" },
+})
