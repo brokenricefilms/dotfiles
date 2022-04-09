9847adc03766b840a77f6ce73e3ff81266874b7c
Thuan Pham <phamtanthuan2311@gmail.com>
2022-04-09 18:47:44 +0700

    🎨refactor(zsh gnome): backup and commit to git server

--- zsh/functions/gnome.zsh
+++ zsh/functions/gnome.zsh
@@ -1,25 +1,32 @@
 function backupGnomeKeyBinding {
-	dconf dump '/org/gnome/desktop/wm/keybindings/' >~/dotfiles/gnome/keybindings.dconf
-	dconf dump '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/' >~/dotfiles/gnome/custom-keybindings.dconf
+  dconf dump '/org/gnome/desktop/wm/keybindings/' >~/dotfiles/gnome/keybindings.dconf
+  dconf dump '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/' >~/dotfiles/gnome/custom-keybindings.dconf
 }
 
 function restoreGnomeKeyBinding {
-	dconf load '/org/gnome/desktop/wm/keybindings/' <~/dotfiles/gnome/keybindings.dconf
-	dconf load '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/' <~/dotfiles/gnome/custom-keybindings.dconf
+  dconf load '/org/gnome/desktop/wm/keybindings/' <~/dotfiles/gnome/keybindings.dconf
+  dconf load '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/' <~/dotfiles/gnome/custom-keybindings.dconf
 }
 
 function backupGnomeTerminal {
-	dconf dump '/org/gnome/terminal/' >~/dotfiles/gnome/gnomeTerminal.dconf
+  dconf dump '/org/gnome/terminal/' >~/dotfiles/gnome/gnomeTerminal.dconf
 }
 
 function restoreGnomeTerminal {
-	dconf load '/org/gnome/terminal/' <~/dotfiles/gnome/gnomeTerminal.dconf
+  dconf load '/org/gnome/terminal/' <~/dotfiles/gnome/gnomeTerminal.dconf
 }
 
 function backupGnomeSetting {
-	dconf dump / >~/dotfiles/gnome/gnomeSetting.dconf
+  cd ~/dotfiles/
+  git stash
+  dconf dump / >~/dotfiles/gnome/gnomeSetting.dconf
+  git add .
+  git commit -m "backup gnome setting"
+  git push
+  git stash pop
+  cd -
 }
 
 function restoreGnomeSetting {
-	dconf load / <~/dotfiles/gnome/gnomeSetting.dconf
+  dconf load / <~/dotfiles/gnome/gnomeSetting.dconf
 }
