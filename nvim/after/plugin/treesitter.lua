c2e2d3e02ac38bccd2469ff9f505255ed08ab7b3
Thuan Pham <phamtanthuan2311@gmail.com>
2022-04-11 22:48:46 +0700

    🎨refactor(nvim/after/plugin/treesitter.lua): use 'all' support for treesitter

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,18 +1,18 @@
-require'nvim-treesitter.configs'.setup {
-    -- One of "all", "maintained" (parsers with maintainers), or a list of languages
-    ensure_installed = "maintained",
+require("nvim-treesitter.configs").setup({
+	-- One of "all", "maintained" (parsers with maintainers), or a list of languages
+	ensure_installed = "all",
 
-    -- Install languages synchronously (only applied to `ensure_installed`)
-    sync_install = false,
+	-- Install languages synchronously (only applied to `ensure_installed`)
+	sync_install = false,
 
-    highlight = {
-        -- `false` will disable the whole extension
-        enable = true,
+	highlight = {
+		-- `false` will disable the whole extension
+		enable = true,
 
-        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-        -- Using this option may slow down your editor, and you may see some duplicate highlights.
-        -- Instead of true it can also be a list of languages
-        additional_vim_regex_highlighting = false
-    }
-}
+		-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+		-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+		-- Using this option may slow down your editor, and you may see some duplicate highlights.
+		-- Instead of true it can also be a list of languages
+		additional_vim_regex_highlighting = false,
+	},
+})
