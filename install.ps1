5bc2e3e2b8fc69123f5273d9b808804dff116010
Thuan Pham <phamtanthuan2311@gmail.com>
2022-05-23 22:58:42 +0700

    fix(install): env userprofile

--- install.ps1
+++ install.ps1
@@ -34,23 +34,23 @@ Install-Module -Name PSFzf -AllowPrerelease -Scope CurrentUser -Force -SkipPubli
 Install-Module -Name Recycle -AllowPrerelease -Scope CurrentUser -Force -SkipPublisherCheck
 Install-Module -Name Terminal-Icons -AllowPrerelease -Scope CurrentUser -Force -SkipPublisherCheck
 
-sudo New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1" -Target "$env:HOMEPATH\dotfiles\powershell\Microsoft.PowerShell_profile.ps1" -Force
+sudo New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1" -Target "$env:USERPROFILE\dotfiles\powershell\Microsoft.PowerShell_profile.ps1" -Force
 
-sudo New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\.gitconfig" -Target "$env:HOMEPATH\dotfiles\git\gitconfig" -Force
-sudo New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\.ssh\config" -Target "$env:HOMEPATH\dotfiles\ssh\config" -Force
+sudo New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\.gitconfig" -Target "$env:USERPROFILE\dotfiles\git\gitconfig" -Force
+sudo New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\.ssh\config" -Target "$env:USERPROFILE\dotfiles\ssh\config" -Force
 
 sudo mkdir $env:appdata\alacritty\
 sudo New-Item -ItemType SymbolicLink -Path "$env:appdata\alacritty\alacritty.yml" -Target "$env:USERPROFILE\dotfiles\alacritty.yml" -Force
 
-sudo New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" -Target "$env:HOMEPATH\dotfiles\windows_terminal_config.json" -Force
+sudo New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" -Target "$env:USERPROFILE\dotfiles\windows_terminal_config.json" -Force
 
-sudo New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\AppData\Local\nvim\init.vim" -Target "$env:HOMEPATH\dotfiles\nvim\init.vim" -Force
+sudo New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\AppData\Local\nvim\init.vim" -Target "$env:USERPROFILE\dotfiles\nvim\init.vim" -Force
 
-sudo New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\AppData\Local\Microsoft\PowerToys\settings.json" -Target "$env:HOMEPATH\dotfiles\PowerToys\settings.json" -Force
-sudo New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\AppData\Local\Microsoft\PowerToys\Keyboard Manager\settings.json" -Target "$env:HOMEPATH\dotfiles\PowerToys\Keyboard Manager\settings.json" -Force
-sudo New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\AppData\Local\Microsoft\PowerToys\Keyboard Manager\default.json" -Target "$env:HOMEPATH\dotfiles\PowerToys\Keyboard Manager\default.json" -Force
+sudo New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\AppData\Local\Microsoft\PowerToys\settings.json" -Target "$env:USERPROFILE\dotfiles\PowerToys\settings.json" -Force
+sudo New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\AppData\Local\Microsoft\PowerToys\Keyboard Manager\settings.json" -Target "$env:USERPROFILE\dotfiles\PowerToys\Keyboard Manager\settings.json" -Force
+sudo New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\AppData\Local\Microsoft\PowerToys\Keyboard Manager\default.json" -Target "$env:USERPROFILE\dotfiles\PowerToys\Keyboard Manager\default.json" -Force
 
-sudo New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\AppData\Roaming\TouchCursor\settings.cfg" -Target "$env:HOMEPATH\dotfiles\TouchCursor\settings.cfg" -Force
-sudo New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\AppData\Roaming\TouchCursor\pre-1.7.1-settings.cfg" -Target "$env:HOMEPATH\dotfiles\TouchCursor\pre-1.7.1-settings.cfg" -Force
+sudo New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\AppData\Roaming\TouchCursor\settings.cfg" -Target "$env:USERPROFILE\dotfiles\TouchCursor\settings.cfg" -Force
+sudo New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\AppData\Roaming\TouchCursor\pre-1.7.1-settings.cfg" -Target "$env:USERPROFILE\dotfiles\TouchCursor\pre-1.7.1-settings.cfg" -Force
 
-sudo New-Item -ItemType SymbolicLink -Path "$env:LOCALAPPDATA\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\LocalState\settings.json" -Target "$env:HOMEPATH\dotfiles\winget\settings.json" -Force
+sudo New-Item -ItemType SymbolicLink -Path "$env:LOCALAPPDATA\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\LocalState\settings.json" -Target "$env:USERPROFILE\dotfiles\winget\settings.json" -Force
