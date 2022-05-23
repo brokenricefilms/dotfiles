ea3a5b7088880b1ddcc6e21340e26dc30fe114e1
Thuan Pham <phamtanthuan2311@gmail.com>
2022-05-23 13:58:13 +0700

    fix(install): env userprofile

--- install.ps1
+++ install.ps1
@@ -10,17 +10,17 @@ Invoke-WebRequest https://deno.land/x/install/install.ps1 -useb | Invoke-Express
 Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
 Invoke-WebRequest get.scoop.sh | Invoke-Expression
 
-scoop install aria2 
+scoop install aria2
 scoop config aria2-warning-enabled false
 
-scoop install git 
+scoop install git
 scoop bucket add extras
 scoop bucket add main
 scoop bucket add nerd-fonts
 scoop bucket add nonportable
 
 scoop bucket add github-gh https://github.com/cli/scoop-gh.git
-scoop install gh gsudo openssh neovim fd ripgrep fzf starship delta python clangd cowsay rust unrar 
+scoop install gh gsudo openssh neovim fd ripgrep fzf starship delta python clangd cowsay rust unrar
 
 scoop install ruby msys2
 ridk install
@@ -34,21 +34,21 @@ Install-Module -Name PSFzf -AllowPrerelease -Scope CurrentUser -Force -SkipPubli
 Install-Module -Name Recycle -AllowPrerelease -Scope CurrentUser -Force -SkipPublisherCheck
 Install-Module -Name Terminal-Icons -AllowPrerelease -Scope CurrentUser -Force -SkipPublisherCheck
 
-sudo New-Item -ItemType SymbolicLink -Path "$env:HOMEPATH\Documents\PowerShell\Microsoft.PowerShell_profile.ps1" -Target "$env:HOMEPATH\dotfiles\powershell\Microsoft.PowerShell_profile.ps1" -Force
+sudo New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1" -Target "$env:HOMEPATH\dotfiles\powershell\Microsoft.PowerShell_profile.ps1" -Force
 
-sudo New-Item -ItemType SymbolicLink -Path "$env:HOMEPATH\.gitconfig" -Target "$env:HOMEPATH\dotfiles\git\gitconfig" -Force
-sudo New-Item -ItemType SymbolicLink -Path "$env:HOMEPATH\.ssh\config" -Target "$env:HOMEPATH\dotfiles\ssh\config" -Force
+sudo New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\.gitconfig" -Target "$env:HOMEPATH\dotfiles\git\gitconfig" -Force
+sudo New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\.ssh\config" -Target "$env:HOMEPATH\dotfiles\ssh\config" -Force
 
 sudo mkdir $env:appdata\alacritty\
 sudo New-Item -ItemType SymbolicLink -Path "$env:appdata\alacritty\alacritty.yml" -Target "$env:USERPROFILE\dotfiles\alacritty.yml" -Force
 
-sudo New-Item -ItemType SymbolicLink -Path "$env:HOMEPATH\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" -Target "$env:HOMEPATH\dotfiles\windows_terminal_config.json" -Force
+sudo New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" -Target "$env:HOMEPATH\dotfiles\windows_terminal_config.json" -Force
 
-sudo New-Item -ItemType SymbolicLink -Path "$env:HOMEPATH\AppData\Local\nvim\init.vim" -Target "$env:HOMEPATH\dotfiles\nvim\init.vim" -Force
+sudo New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\AppData\Local\nvim\init.vim" -Target "$env:HOMEPATH\dotfiles\nvim\init.vim" -Force
 
-sudo New-Item -ItemType SymbolicLink -Path "$env:HOMEPATH\AppData\Local\Microsoft\PowerToys\settings.json" -Target "$env:HOMEPATH\dotfiles\PowerToys\settings.json" -Force
-sudo New-Item -ItemType SymbolicLink -Path "$env:HOMEPATH\AppData\Local\Microsoft\PowerToys\Keyboard Manager\settings.json" -Target "$env:HOMEPATH\dotfiles\PowerToys\Keyboard Manager\settings.json" -Force
-sudo New-Item -ItemType SymbolicLink -Path "$env:HOMEPATH\AppData\Local\Microsoft\PowerToys\Keyboard Manager\default.json" -Target "$env:HOMEPATH\dotfiles\PowerToys\Keyboard Manager\default.json" -Force
+sudo New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\AppData\Local\Microsoft\PowerToys\settings.json" -Target "$env:HOMEPATH\dotfiles\PowerToys\settings.json" -Force
+sudo New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\AppData\Local\Microsoft\PowerToys\Keyboard Manager\settings.json" -Target "$env:HOMEPATH\dotfiles\PowerToys\Keyboard Manager\settings.json" -Force
+sudo New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\AppData\Local\Microsoft\PowerToys\Keyboard Manager\default.json" -Target "$env:HOMEPATH\dotfiles\PowerToys\Keyboard Manager\default.json" -Force
 
-sudo New-Item -ItemType SymbolicLink -Path "$env:HOMEPATH\AppData\Roaming\TouchCursor\settings.cfg" -Target "$env:HOMEPATH\dotfiles\TouchCursor\settings.cfg" -Force
-sudo New-Item -ItemType SymbolicLink -Path "$env:HOMEPATH\AppData\Roaming\TouchCursor\pre-1.7.1-settings.cfg" -Target "$env:HOMEPATH\dotfiles\TouchCursor\pre-1.7.1-settings.cfg" -Force
+sudo New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\AppData\Roaming\TouchCursor\settings.cfg" -Target "$env:HOMEPATH\dotfiles\TouchCursor\settings.cfg" -Force
+sudo New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\AppData\Roaming\TouchCursor\pre-1.7.1-settings.cfg" -Target "$env:HOMEPATH\dotfiles\TouchCursor\pre-1.7.1-settings.cfg" -Force
