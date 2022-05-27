b9d489df77173f6c242a1e14f0c044ea7011f383
Thuan Pham <phamtanthuan2311@gmail.com>
2022-05-28 06:50:08 +0700

    🎨refactor(nvim/after/plugin/formatter.lua): update packet for distros

--- zsh/functions/update.zsh
+++ zsh/functions/update.zsh
@@ -1,3 +1,40 @@
+# for debian base
+if hash apt 2>/dev/null; then
+  alias ins='sudo apt install -y'
+  alias inss='apt search'
+  alias uins='sudo apt remove -y'
+fi
+
+# for fedora
+if hash dnf 2>/dev/null; then
+  alias ins='sudo dnf -y install'
+  alias inss='dnf search'
+  alias uins='sudo dnf -y remove'
+fi
+
+# for arch base
+if hash pacman 2>/dev/null; then
+  alias ins='sudo pacman -S --noconfirm'
+  alias inss='pacman -Ss'
+  alias ya='yay --noconfirm'
+  alias uins='sudo pacman -Rs --noconfirm'
+
+  function switchToJava8() {
+    sudo archlinux-java set java-8-openjdk
+    archlinux-java status
+  }
+
+  function switchToJava11() {
+    sudo archlinux-java set java-8-openjdk
+    archlinux-java status
+  }
+
+  function switchToJava17() {
+    sudo archlinux-java set java-17-openjdk
+    archlinux-java status
+  }
+fi
+
 function updateZshPlugin() {
   echo "update zsh plugin"
 
@@ -15,8 +52,20 @@ function updateZshPlugin() {
 }
 
 function update() {
-  # @todo: support other distro
-  sudo dnf upgrade -y
+  # for debian base
+  if hash apt 2>/dev/null; then
+    sudo apt update -y
+  fi
+
+  # for fedora
+  if hash dnf 2>/dev/null; then
+    sudo dnf upgrade -y
+  fi
+
+  # for arch base
+  if hash pacman 2>/dev/null; then
+    sudo pacman --sync --refresh --sysupgrade
+  fi
 
   nvim -c "PlugUpdate | qa"
 
