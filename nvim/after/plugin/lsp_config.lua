9167ba736a76a569b54c54119d1739e52ca2c573
Thuan Pham <phamtanthuan2311@gmail.com>
2022-05-30 16:44:21 +0700

    🌟feature(nvim lsp): run js lsp everwhere

--- nvim/after/plugin/lsp_config.lua
+++ nvim/after/plugin/lsp_config.lua
@@ -11,6 +11,13 @@ lsp_installer.on_server_ready(function(server)
 		}
 	end
 
+	require("lspconfig").tsserver.setup({
+		filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
+		root_dir = function()
+			return vim.loop.cwd()
+		end, -- run lsp for javascript in any directory
+	})
+
 	server:setup(opts)
 end)
 
