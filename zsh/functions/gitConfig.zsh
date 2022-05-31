fba1f8c4f277d925564425ab9b220ec97b84aa57
Thuan Pham <phamtanthuan2311@gmail.com>
2022-05-31 23:56:18 +0700

    🌟feature(gitConfig.zsh): pushf aka git pull &; git push -f &

--- zsh/functions/gitConfig.zsh
+++ zsh/functions/gitConfig.zsh
@@ -2,6 +2,7 @@
 
 alias add='git add'
 alias push="git pull & ; git push &"
+alias pushf="git pull & ; git push -f &"
 alias pull="git pull &"
 alias dv='git difftool'
 alias gl='git log --oneline | head -n 5'
