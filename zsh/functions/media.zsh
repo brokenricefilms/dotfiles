ab707abe02997cdd190647c8373d3b57b2e79d77
Thuan Pham <phamtanthuan2311@gmail.com>
2022-06-16 09:50:26 +0700

    🎨refactor(media.zsh): rename function

--- zsh/functions/media.zsh
+++ zsh/functions/media.zsh
@@ -2,7 +2,7 @@ alias m='mpv --shuffle --loop-playlist ~/Music/*'
 alias yt='yt-dlp -f bestvideo+bestaudio'
 alias yta='yt-dlp -f "bestaudio" --continue --no-overwrites --ignore-errors --extract-audio --audio-format mp3 -o "%(title)s.%(ext)s"'
 
-function syncMusic() {
+function update_music() {
   cd ~/Music/
   trash *
   yta "https://thuanpham2311.github.io/l/music"
