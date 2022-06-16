e7cfa4e7289e2b6b56ff9b6050901f458162ca37
Thuan Pham <phamtanthuan2311@gmail.com>
2022-06-16 09:56:35 +0700

    🎨refactor(zsh): auto install yt-dlp

--- zsh/functions/media.zsh
+++ zsh/functions/media.zsh
@@ -1,6 +1,13 @@
+if hash yt-dlp 2>/dev/null; then
+  alias yt='yt-dlp -f bestvideo+bestaudio'
+  alias yta='yt-dlp -f "bestaudio" --continue --no-overwrites --ignore-errors --extract-audio --audio-format mp3 -o "%(title)s.%(ext)s"'
+else
+  python3 -m pip install -U yt-dlp
+  alias yt='yt-dlp -f bestvideo+bestaudio'
+  alias yta='yt-dlp -f "bestaudio" --continue --no-overwrites --ignore-errors --extract-audio --audio-format mp3 -o "%(title)s.%(ext)s"'
+fi
+
 alias m='mpv --shuffle --loop-playlist ~/Music/*'
-alias yt='yt-dlp -f bestvideo+bestaudio'
-alias yta='yt-dlp -f "bestaudio" --continue --no-overwrites --ignore-errors --extract-audio --audio-format mp3 -o "%(title)s.%(ext)s"'
 
 function update_music() {
   cd ~/Music/
