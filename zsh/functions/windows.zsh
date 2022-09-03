b10ce7316e32ad53bf81ab1e82133a5c8372090c
Thuan Pham <phamtanthuan2311@gmail.com>
2022-09-03 17:44:53 +0700

    🎨refactor(zsh/windows): taskkill, tasklist

--- zsh/functions/windows.zsh
+++ zsh/functions/windows.zsh
@@ -2,3 +2,9 @@ alias pwsh='pwsh.exe'
 alias notepad='notepad.exe'
 alias winget='winget.exe'
 alias explorer='explorer.exe'
+
+function windowsTaskKill() {
+  task=$(tasklist.exe | fzf | awk '{print $1}')
+  taskkill.exe /f /im $task
+}
+alias tk=windowsTaskKill
