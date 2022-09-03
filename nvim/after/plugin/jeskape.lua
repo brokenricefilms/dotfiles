50ac2b7663dd2df904a3fe96711b0f14c80e3e33
Thuan Pham <phamtanthuan2311@gmail.com>
2022-09-03 17:39:01 +0700

    🎨refactor(nvim/after/plugin/jeskape.lua): minimal

--- nvim/after/plugin/jeskape.lua
+++ nvim/after/plugin/jeskape.lua
@@ -4,13 +4,8 @@ require("jeskape").setup({
 			p = "<cmd>call fzf#vim#complete#path('fd')<enter>",
 			w = "<cmd>call fzf#vim#complete#word({'window': { 'width': 0.2, 'height': 0.9, 'xoffset': 1 }})<enter>",
 		},
-		k = {
-			k = "<esc>",
-			j = "<esc>",
-		},
 		j = {
 			j = "<esc>",
-			k = "<esc>",
 		},
 		timeout = vim.o.timeoutlen,
 	},
