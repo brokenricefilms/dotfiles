c827e5f4ce21f2a9c7eae7924617ecb2611fd23e
Thuan Pham <phamtanthuan2311@gmail.com>
2022-09-11 20:14:50 +0700

    🎨refactor(install.sh): npm, clean

--- install.sh
+++ install.sh
@@ -25,23 +25,19 @@ if hash dnf 2>/dev/null; then
   sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y
   sudo dnf groupupdate sound-and-video -y
 
-  sudo dnf copr enable atim/lazygit -y
-
   sudo dnf config-manager --add-repo https://download.opensuse.org/repositories/home:lamlng/Fedora_33/home:lamlng.repo
 
   sudo dnf copr enable sunwire/input-remapper -y
   sudo dnf install python3-input-remapper -y
 
-  sudo dnf install tmux curl wget git neofetch htop gnome-tweaks trash-cli flameshot python3-pip tldr net-tools xclip speedtest-cli neovim python3-neovim fd-find aria2 tree cowsay fzf npm ffmpeg youtube-dl mpv tig kitty ripgrep unrar exa moreutils foliate bat util-linux-user zsh cronie git-delta wl-clipboard java-devel git-clang-format rust cargo go gtk-v4l ruby ruby-devel google-chrome-stable gcc-c++ ibus-bamboo gnome-extensions-app lazygit collectd-sensors olive obs-studio dconf-editor sqlite shfmt -y
-
-  curl -fsSL https://deno.land/install.sh | sh
+  sudo dnf install tmux curl wget git neofetch htop gnome-tweaks trash-cli flameshot python3-pip tldr net-tools xclip speedtest-cli neovim python3-neovim fd-find aria2 tree cowsay fzf npm ffmpeg youtube-dl mpv tig kitty ripgrep unrar exa moreutils foliate bat util-linux-user zsh cronie git-delta wl-clipboard java-devel git-clang-format rust cargo go gtk-v4l ruby ruby-devel google-chrome-stable gcc-c++ ibus-bamboo gnome-extensions-app collectd-sensors olive obs-studio dconf-editor sqlite shfmt -y
 fi
 
 # for arch base
 if hash pacman 2>/dev/null; then
   sudo pacman-mirrors --geoip && sudo pacman -Syyu
 
-  sudo pacman -S firefox-developer-edition tmux neofetch htop mpv trash-cli flameshot tldr net-tools xclip speedtest-cli neovim fd tree gimp cowsay ruby ripgrep kitty exa noto-fonts-emoji lua aria2 foliate tig clang nodejs bun youtube-dl bat python-pip git base-devel wget curl fzf dunst python-i3ipc xdg-desktop-portal xdg-desktop-portal-gnome wmctrl thefuck wl-clipboard --noconfirm
+  sudo pacman -S firefox-developer-edition tmux neofetch htop mpv trash-cli flameshot tldr net-tools xclip speedtest-cli neovim fd tree gimp cowsay ruby ripgrep kitty exa noto-fonts-emoji lua aria2 foliate tig clang nodejs youtube-dl bat python-pip git base-devel wget curl fzf dunst python-i3ipc xdg-desktop-portal xdg-desktop-portal-gnome wmctrl thefuck wl-clipboard --noconfirm
 
   git clone https://aur.archlinux.org/yay.git
   cd yay
@@ -51,20 +47,18 @@ if hash pacman 2>/dev/null; then
 
 fi
 
-curl https://bun.sh/install | bash
+curl -fsSL https://deno.land/install.sh | sh
 
-[ -s "/home/master/.bun/_bun" ] && source "/home/master/.bun/_bun"
-export BUN_INSTALL="/home/master/.bun"
-export PATH="$BUN_INSTALL/bin:$PATH"
+npm config set prefix ~/.npm/
 
-bun install --global prettier
-bun install --global tinypng-cli
-bun install --global browser-sync
-bun install --global yarn
-bun install --global neovim
-bun install --global typescript
-bun install --global mathjs
-bun install --global bash-language-server
+npm install --global prettier
+npm install --global tinypng-cli
+npm install --global browser-sync
+npm install --global yarn
+npm install --global neovim
+npm install --global typescript
+npm install --global mathjs
+npm install --global bash-language-server
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 
@@ -74,6 +68,9 @@ pip install --upgrade pynvim
 
 gem install neovim jekyll bundler
 cargo install stylua
+
+go install github.com/jesseduffield/lazygit@latest
+go install github.com/jesseduffield/lazydocker@latest
 
 # nvim setup
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
@@ -144,7 +141,7 @@ ln -sf ~/sync/ok/ibus-bamboo.macro.text ~/.config/ibus-bamboo/
 
 mkdir ~/repos/
 cd ~/repos/
-gh repo clone thuanpham2311/viet_eng_words_vim_dict
+git clone git@github.com:thuanpham2311/viet_eng_words_vim_dict
 cd -
 
 sudo ln -sf ~/repos/viet_eng_words_vim_dict/words /usr/share/dict/words
