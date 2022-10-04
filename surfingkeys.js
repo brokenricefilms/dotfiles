23abddf456fabd24212ee75d3b425654a5af163c
Thuan Pham <phamtanthuan2311@gmail.com>
2022-10-04 10:03:53 +0700

    🎨refactor(surfingkeys): github theme

--- surfingkeys.js
+++ surfingkeys.js
@@ -15,32 +15,51 @@ settings.theme = `
 .sk_theme {
   font-family: JetBrains Mono NL, Cascadia Code, SauceCodePro Nerd Font, Consolas, Menlo, monospace;
   font-size: 10pt;
-  background: #f0edec;
-  color: #353535;
+  background: #ffffff;
+  color: #24292f;
 }
 .sk_theme tbody {
-  color: #eeeeee;
+  color: #ffffff;
 }
 .sk_theme input {
-  color: #353535;
+  color: #24292f;
 }
 .sk_theme .url {
-  color: #1d5573;
+  color: #24292f;
 }
 .sk_theme .annotation {
-  color: #353535;
+  color: #24292f;
 }
 .sk_theme .omnibar_highlight {
-  color: #88507d;
+  color: #24292f;
 }
 .sk_theme #sk_omnibarSearchResult ul li:nth-child(odd) {
-  background: #eeeeee;
+  background: #ffffff;
 }
 .sk_theme #sk_omnibarSearchResult ul li.focused {
-  background: #3b8992;
+  background: #0598bc;
 }
 #sk_status,
 #sk_find {
   font-size: 10pt;
 }
 `;
+
+// #ffffff
+// #24292f
+// #24292e
+// #d73a49
+// #28a745
+// #dbab09
+// #0366d6
+// #5a32a3
+// #0598bc
+// #6a737d
+// #959da5
+// #cb2431
+// #22863a
+// #b08800
+// #005cc5
+// #5a32a3
+// #3192aa
+// #d1d5da
