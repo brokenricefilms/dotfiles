b88e5a7abcf8a26e07d0fcea29861931c2234de1
Thuan Pham <phamtanthuan2311@gmail.com>
2022-10-10 10:21:00 +0700

    🎨refactor(nvim/plugin/lsp_config.lua): nvim lint config

--- nvim/plugin/lsp_config.lua
+++ nvim/plugin/lsp_config.lua
@@ -136,3 +136,9 @@ require("lspconfig").phan.setup({})
 require("lint").linters_by_ft = {
 	markdown = { "markdownlint" },
 }
+
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+	callback = function()
+		require("lint").try_lint()
+	end,
+})
