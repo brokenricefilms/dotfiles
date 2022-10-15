f2e780bc87f34267ff9c9be5225c5f48b556c978
Thuan Pham <phamtanthuan2311@gmail.com>
2022-10-15 17:50:22 +0700

    🐞fix(nvim): tsserver suck

--- nvim/plugin/lsp_config.lua
+++ nvim/plugin/lsp_config.lua
@@ -70,10 +70,11 @@ require("lspconfig")["pyright"].setup({
 	flags = lsp_flags,
 })
 
-require("lspconfig")["tsserver"].setup({
-	on_attach = on_attach,
-	flags = lsp_flags,
-})
+-- deno?
+-- require("lspconfig")["tsserver"].setup({
+-- on_attach = on_attach,
+-- flags = lsp_flags,
+-- })
 
 require("lspconfig")["rust_analyzer"].setup({
 	on_attach = on_attach,
