7cc18ca96eec9e45a9bc11284c932508a0db2a98
Thuan Pham <phamtanthuan2311@gmail.com>
2022-10-15 19:10:51 +0700

    ref(input-remapper): update

--- nvim/plugin/lsp_config.lua
+++ nvim/plugin/lsp_config.lua
@@ -1,21 +1,3 @@
-local border = {
-	{ "╭", "FloatBorder" },
-	{ "─", "FloatBorder" },
-	{ "╮", "FloatBorder" },
-	{ "│", "FloatBorder" },
-	{ "╯", "FloatBorder" },
-	{ "─", "FloatBorder" },
-	{ "╰", "FloatBorder" },
-	{ "│", "FloatBorder" },
-}
-
-local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
-function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
-	opts = opts or {}
-	opts.border = opts.border or border
-	return orig_util_open_floating_preview(contents, syntax, opts, ...)
-end
-
 local keymap = vim.keymap.set
 local saga = require("lspsaga")
 
