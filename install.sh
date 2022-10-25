6ac2df2a7c64eba80b8683bbf08b84898dd55118
Thuan Pham <thuanOwa@gmail.com>
2022-10-25 18:11:17 +0700

    ubuntu

--- install.sh
+++ install.sh
@@ -1,21 +1,13 @@
 #! /usr/bin/env sh
 
-echo "setup gh cli ssh first"
+sudo apt install apt-transport-https curl gnupg -y
 
-sudo dnf install dnf-plugins-core -y
+sudo add-apt-repository ppa:bamboo-engine/ibus-bamboo -y
+sudo add-apt-repository ppa:apandada1/foliate -y
 
-sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-sudo dnf groupupdate core -y
+sudo apt update -y
 
-sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y
-sudo dnf groupupdate sound-and-video -y
-
-sudo dnf config-manager --add-repo https://download.opensuse.org/repositories/home:lamlng/Fedora_33/home:lamlng.repo
-
-sudo dnf copr enable sunwire/input-remapper -y
-sudo dnf install python3-input-remapper -y
-
-sudo dnf install -y tmux curl wget git neofetch htop gnome-tweaks trash-cli flameshot python3-pip tldr net-tools xclip speedtest-cli neovim python3-neovim fd-find aria2 tree cowsay fzf npm ffmpeg youtube-dl mpv ripgrep unrar exa moreutils foliate bat util-linux-user zsh cronie git-delta wl-clipboard java-devel git-clang-format rust cargo go gtk-v4l ruby ruby-devel xdg-open-stable gcc-c++ ibus-bamboo gnome-extensions-app collectd-sensors  obs-studio dconf-editor sqlite shfmt v4l-utils google-noto-emoji-color-fonts cmake kdenlive
+sudo apt install copyq zsh tmux curl wget git neofetch htop mpv gnome-tweaks trash-cli flameshot ipython3 python3-pip tldr net-tools speedtest-cli neovim fd-find aria2 tree cowsay fzf clangd golang fonts-noto-mono ripgrep foliate zsh ibus-bamboo unrar moreutils exa universal-ctags lua5.4 node-typescript bat python3-venv shfmt unzip cargo gem ruby-full build-essential zlib1g-dev php adb delta npm libsqlite3-dev -y
 
 curl -fsSL https://deno.land/install.sh | sh
 
@@ -43,12 +35,6 @@ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
 
 npm config set prefix ~/.npm/
 
-cd /tmp
-wget https://github.com/xo/usql/releases/download/v0.12.13/usql-0.12.13-linux-amd64.tar.bz2
-mv usql ~/.local/bin/
-
-usql -c '\set SYNTAX_HL_STYLE github'
-
 cd ~/
 git clone git@github.com:thuanOwa/dotfiles.git
 
@@ -56,11 +42,6 @@ mkdir ~/sync/
 cd ~/sync/
 git clone git@github.com:thuanOwa/ok.git
 git clone git@github.com:thuanOwa/obs-studio.git
-
-cd /tmp
-wget https://github.com/ful1e5/Google_Cursor/releases/download/v1.1.3/GoogleDot-Blue.tar.gz
-tar -xvf GoogleDot-Blue.tar.gz
-sudo mv GoogleDot-Blue/ /usr/share/icons/
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
