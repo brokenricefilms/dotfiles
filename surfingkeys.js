bfe7ea582da9e91c62b4f8b62de8c0fff8606b93
Thuan Pham <thuanOwa@gmail.com>
2022-10-27 14:13:40 +0700

    clean(surfingkeys): stuff

--- surfingkeys.js
+++ surfingkeys.js
@@ -1,35 +1,15 @@
-// #ffffff
-// #24292f
-// #24292e
-// #d73a49
-// #28a745
-// #dbab09
-// #0366d6
-// #5a32a3
-// #0598bc
-// #6a737d
-// #959da5
-// #cb2431
-// #22863a
-// #b08800
-// #005cc5
-// #5a32a3
-// #3192aa
-// #d1d5da
-
 settings.modeAfterYank = "Normal";
 settings.scrollStepSize = 100;
 
 // Disable emoji
 api.iunmap(":");
 
-api.Hints.characters = "asdghklqwertyuiopzxcvbnmfj1234567890;/.,][-=`";
+api.Hints.characters = "asdghklqwertyuiopzxcvbnmfj1234567890";
 
 const hintsCss =
   "font-size: 12pt; font-family: JetBrains Mono NL, Cascadia Code, SauceCodePro Nerd Font, Consolas, Menlo, monospace; border: 0px; color: #0366d6; background: initial; background-color: #ffffff";
 
 api.Hints.style(hintsCss);
-api.Hints.style(hintsCss, "text");
 
 settings.theme = `
 .sk_theme {
