6bbf416e3efd412c8647fe515f427c7cbe6ae3bb
Thuan Pham <thuanOwa@gmail.com>
2022-10-28 22:04:23 +0700

    fix(nviM): wls clipboard

--- install.sh
+++ install.sh
@@ -35,6 +35,12 @@ go install github.com/jesseduffield/lazydocker@latest
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+sudo apt remove xclip -y
+curl -sLo/tmp/win32yank.zip https://github.com/equalsraf/win32yank/releases/download/v0.0.4/win32yank-x64.zip
+unzip -p /tmp/win32yank.zip win32yank.exe >/tmp/win32yank.exe
+chmod +x /tmp/win32yank.exe
+sudo mv /tmp/win32yank.exe /usr/local/bin/
+
 npm config set prefix ~/.npm/
 
 cd ~/
