c34343364ae97d299aeb300deeb8526d6e5d49a8
Thuan Pham <thuanowa@gmail.com>
2022-11-26 19:29:25 +0700

    style: format

--- init.lua
+++ init.lua
@@ -36,11 +36,14 @@ vim.cmd [[colorscheme zellner]]
 opt.shortmess:append "sI"
 
 local function map(mode, lhs, rhs, opts)
-  local options = { noremap = true, silent = true }
-  if opts then
-    options = vim.tbl_extend('force', options, opts)
-  end
-  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+    local options = {
+        noremap = true,
+        silent = true
+    }
+    if opts then
+        options = vim.tbl_extend('force', options, opts)
+    end
+    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
 vim.g.mapleader = ' '
