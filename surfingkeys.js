24399b4879056fd5faaf2bbaf41b1fbaa2b652c2
Thuan Pham <thuanowa@gmail.com>
2022-12-01 18:52:11 +0700

    ref(git): yo no push

--- surfingkeys.js
+++ surfingkeys.js
@@ -11,7 +11,7 @@ api.iunmap(":");
 api.Hints.characters = "asdghklqwertyuiopzxcvbnmfj1234567890";
 
 const hintsCss =
-  "font-size: 12pt; font-family: JetBrains Mono NL, Cascadia Code, SauceCodePro Nerd Font, Consolas, Menlo, monospace; border: 0px; color: #0366d6; background: initial; background-color: #ffffff";
+  "font-size: 12pt; font-family: JetBrains Mono NL, Cascadia Code, SauceCodePro Nerd Font, Consolas, Menlo, monospace; border: 0px; color: #ffffff; background: initial; background-color: #000000";
 
 api.Hints.style(hintsCss);
 api.Hints.style(hintsCss, "text");
@@ -20,26 +20,26 @@ settings.theme = `
 .sk_theme {
   font-family: JetBrains Mono NL, Cascadia Code, SauceCodePro Nerd Font, Consolas, Menlo, monospace;
   font-size: 10pt;
-  background: #ffffff;
-  color: #24292f;
-}
-.sk_theme tbody {
+  background: #0000000;
   color: #ffffff;
 }
+.sk_theme tbody {
+  color: #0000000;
+}
 .sk_theme input {
-  color: #24292f;
+  color: #ffffff;
 }
 .sk_theme .url {
-  color: #24292f;
+  color: #ffffff;
 }
 .sk_theme .annotation {
-  color: #24292f;
+  color: #ffffff;
 }
 .sk_theme .omnibar_highlight {
-  color: #24292f;
+  color: #ffffff;
 }
 .sk_theme #sk_omnibarSearchResult ul li:nth-child(odd) {
-  background: #ffffff;
+  background: #0000000;
 }
 .sk_theme #sk_omnibarSearchResult ul li.focused {
   background: #0598bc;
