ae12dcda8fbd69cbbe056e82da26d1c8f9a32793
Thuan Pham <thuanowa@gmail.com>
2023-01-23 12:25:30 +0700

    fix(install): wrong path

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env sh
 sudo dnf install dnf-plugins-core -y
 
-sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf groupupdate core -y
 
 sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y
@@ -17,8 +17,8 @@ sudo dnf install starship -y
 
 sudo dnf install -y tmux curl wget git neofetch htop gnome-tweaks trash-cli python3-pip tldr net-tools speedtest-cli neovim python3-neovim fd-find aria2 tree cowsay fzf npm ffmpeg youtube-dl mpv ripgrep unrar moreutils foliate util-linux-user zsh cronie git-delta wl-clipboard java-devel git-clang-format rust cargo go gtk-v4l ruby ruby-devel gcc-c++ ibus-bamboo collectd-sensors obs-studio dconf-editor sqlite shfmt v4l-utils google-noto-emoji-color-fonts cmake kdenlive starship glib2-static libgda libgda-sqlite exa bat libsqlite3x-devel
 
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak install flathub org.onlyoffice.desktopeditors
+flatpak remote-add --authenticator-install --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install --noninteractive flathub org.onlyoffice.desktopeditors
 
 curl -fsSL https://deno.land/install.sh | sh
 
@@ -47,13 +47,7 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.2
 . $HOME/.asdf/asdf.sh
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-asdf plugin add rust
 asdf install nodejs latest
-asdf install rust latest
-asdf install rust-analyzer latest
-
-cd
-gh repo clone thuanowa/dotfiles
 
 ln -sf ~/repos/thuanowa/ok/.tinypng ~/.tinypng
 ln -sf ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
@@ -73,14 +67,6 @@ ln -sf ~/dotfiles/bashrc ~/.bashrc
 ln -sf ~/dotfiles/input-remapper/ ~/.config/
 ln -sf ~/repos/thuanowa/obs-studio/ ~/.config/
 
-mkdir ~/repos/thuanowa
-
-cd ~/repos/thuanowa/
-gh repo clone thuanowa/ok
-ln -sf ~/repos/thuanowa/ok/.fonts ~/.fonts
-cd ~/.fonts
-fc-cache -v
-
 cd ~/repos/thuanowa/
 gh repo list thuanowa --limit 1000 | while read -r repo _; do
   gh repo clone "$repo" "$repo"
@@ -98,8 +84,13 @@ gh repo list isekaiSystem --limit 1000 | while read -r repo _; do
   gh repo clone "$repo" "$repo"
 done
 
+ln -sf ~/repos/thuanowa/ok/.fonts ~/.fonts
+cd ~/.fonts
+fc-cache -v
+
 mkdir ~/app/
 cd ~/app/
+
 git clone https://github.com/swsnr/gnome-search-providers-vscode
 cd gnome-search-providers-vscode
 make build
