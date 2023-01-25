c332dac10b803268b758a30ba68e169c0cbdd132
Thuan Pham <thuanowa@gmail.com>
2023-01-25 17:19:07 +0700

    ref: reinstall fedora

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #! /usr/bin/env sh
-sudo dnf install dnf-plugins-core -y
 
+sudo dnf remove gnome-contacts gnome-weather libreoffice-core gnome-maps gnome-tour rhythmbox gnome-calendar -y
+
+sudo dnf install dnf-plugins-core -y
 sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf groupupdate core -y
 
@@ -15,7 +17,7 @@ sudo dnf install python3-input-remapper -y
 sudo dnf copr enable atim/starship -y
 sudo dnf install starship -y
 
-sudo dnf install -y tmux curl wget git neofetch htop gnome-tweaks trash-cli python3-pip tldr net-tools speedtest-cli neovim python3-neovim fd-find aria2 tree cowsay fzf npm ffmpeg youtube-dl mpv ripgrep unrar moreutils foliate util-linux-user zsh cronie git-delta wl-clipboard java-devel git-clang-format rust cargo go gtk-v4l ruby ruby-devel gcc-c++ ibus-bamboo collectd-sensors obs-studio dconf-editor sqlite shfmt v4l-utils google-noto-emoji-color-fonts cmake kdenlive starship glib2-static libgda libgda-sqlite exa bat libsqlite3x-devel
+sudo dnf install -y tmux curl wget git neofetch htop gnome-tweaks trash-cli python3-pip tldr net-tools speedtest-cli neovim python3-neovim fd-find aria2 tree cowsay fzf npm ffmpeg youtube-dl mpv ripgrep unrar moreutils foliate util-linux-user zsh cronie git-delta wl-clipboard java-devel git-clang-format rust cargo go gtk-v4l ruby ruby-devel gcc-c++ ibus-bamboo collectd-sensors obs-studio dconf-editor sqlite shfmt v4l-utils google-noto-emoji-color-fonts cmake kdenlive starship glib2-static libgda libgda-sqlite exa bat libsqlite3x-devel kitty
 
 flatpak remote-add --authenticator-install --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install --noninteractive flathub org.onlyoffice.desktopeditors
@@ -56,16 +58,18 @@ ln -sf ~/dotfiles/nvim ~/.config/
 ln -sf ~/dotfiles/.tool-versions ~/
 ln -sf ~/dotfiles/git/gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/git/gh_config.yml ~/.config/gh/config.yml
-rm -rf ~/.config/lazygit
-mkdir ~/.config/lazygit
-ln -sf ~/dotfiles/git/lazygit_config.yml ~/.config/lazygit/config.yml
+rm -rf ~/.config/lazygit ; mkdir ~/.config/lazygit ; ln -sf ~/dotfiles/git/lazygit_config.yml ~/.config/lazygit/config.yml
 ln -sf ~/dotfiles/.selected_editor ~/.selected_editor
 ln -sf ~/dotfiles/.ripgreprc ~/
 ln -sf ~/dotfiles/bashrc ~/.bashrc
-ln -sf ~/dotfiles/input-remapper/ ~/.config/
+rm -rf ~/.config/input-remapper/ ; ln -sf ~/dotfiles/input-remapper/ ~/.config/
 ln -sf ~/repos/thuanowa/obs-studio/ ~/.config/
+rm -rf ~/.config/kitty
+ln -sf ~/dotfiles/kitty ~/.config
 
-cd ~/repos/thuanowa/
+mkdir ~/repos/
+
+cd ~/repos/
 gh repo list thuanowa --limit 1000 | while read -r repo _; do
   gh repo clone "$repo" "$repo"
 done
@@ -90,12 +94,4 @@ done
 
 ln -sf ~/repos/thuanowa/ok/.fonts ~/.fonts
 cd ~/.fonts
-fc-cache -v
-
-mkdir ~/app/
-cd ~/app/
-
-git clone https://github.com/swsnr/gnome-search-providers-vscode
-cd gnome-search-providers-vscode
-make build
-sudo make build install
+fc-cache -rf
