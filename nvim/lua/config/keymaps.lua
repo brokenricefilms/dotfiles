07c3dbb14f0c9a3b06d2b084f8368b7987d5b11d
Thuan Pham <thuanowa@gmail.com>
2023-01-28 08:23:12 +0700

    ref(nvim): arrowkey to adjust the windows size

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("", "<Right>", "<C-w><")
+vim.keymap.set("", "<Down>", "<C-w>-")
+vim.keymap.set("", "<Up>", "<C-w>+")
+vim.keymap.set("", "<Left>", "<C-w>>")
