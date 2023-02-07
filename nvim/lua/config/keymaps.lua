a2d7b45697b47102b3bcbf72ceeddf926e9e4f59
Thuan Pham <thuanowa@gmail.com>
2023-02-07 18:09:16 +0700

    chore: lazyvim

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,10 +2,10 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("", "<Right>", "<C-w><")
-vim.keymap.set("", "<Down>", "<C-w>-")
-vim.keymap.set("", "<Up>", "<C-w>+")
-vim.keymap.set("", "<Left>", "<C-w>>")
+vim.keymap.set("", "<S-Right>", "<C-w><")
+vim.keymap.set("", "<S-Down>", "<C-w>-")
+vim.keymap.set("", "<S-Up>", "<C-w>+")
+vim.keymap.set("", "<S-Left>", "<C-w>>")
 
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
