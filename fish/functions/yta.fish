bb5dfcd7da55a68daa9423b61d15f901fb253c7a
Thuan Pham <thuanowa@gmail.com>
2023-02-13 17:30:00 +0700

    ref(fish): rm yta.fish

--- fish/functions/yta.fish
+++
@@ -1,3 +0,0 @@
-function yta --description 'youtube download audio'
-    yt-dlp -f bestaudio --continue --no-overwrites --ignore-errors --extract-audio --audio-format mp3 -o "%(title)s.%(ext)s" $argv
-end
