1c69f9b320721f5b70ae852f37065ee138156a50
Thuan Pham <thuanowa@gmail.com>
2023-02-18 06:16:11 +0700

    refactor(hi.fish): check only one email

--- fish/functions/hi.fish
+++ fish/functions/hi.fish
@@ -1,7 +1,6 @@
 function hi --description 'daily jobs'
     open "https://www.inoreader.com/all_articles"
     open "https://mail.google.com/mail/u/0/#inbox"
-    open "https://mail.google.com/mail/u/1/#inbox"
     open "https://www.youtube.com/feed/subscriptions"
     open "https://github.com"
 
