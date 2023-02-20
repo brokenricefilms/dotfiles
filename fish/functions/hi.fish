028a34db94dd6daa27d15b4617269cbb269879c6
Thuan Pham <thuanowa@gmail.com>
2023-02-20 16:33:09 +0700

    refactor(fish): gmail it's open all time

--- fish/functions/hi.fish
+++ fish/functions/hi.fish
@@ -1,6 +1,5 @@
 function hi --description 'daily jobs'
     open "https://www.inoreader.com/all_articles"
-    open "https://mail.google.com/mail/u/0/#inbox"
     open "https://www.youtube.com/feed/subscriptions"
     open "https://github.com"
 
