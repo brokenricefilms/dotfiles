5cf40a6f920e2b4820efac22fd1528acb8d26c41
Thuan Pham <thuanowa@gmail.com>
2023-02-23 18:58:39 +0700

    refactor(nvim): no snippet

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -11,29 +11,3 @@ vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("n", "*", "*zz")
 vim.keymap.set("n", "#", "#zz")
-
-local ls = require("luasnip")
-
-ls.add_snippets(
-  "all", {
-  ls.snippet("todo", {
-    ls.insert_node(0),
-    -- TODO: comment syntax
-    ls.text_node("TODO: "),
-    ls.insert_node(1),
-  }),
-})
-
-ls.add_snippets(
-  "fish", {
-  ls.snippet("f", {
-    ls.text_node("function "),
-    ls.insert_node(1),
-    ls.text_node(' --description "'),
-    ls.insert_node(2),
-    ls.text_node('"'),
-    ls.text_node({ "", "  " }),
-    ls.insert_node(3),
-    ls.text_node({ "", "end" })
-  }),
-})
