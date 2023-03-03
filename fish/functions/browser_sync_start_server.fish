54badf53fe2c0bbb38779a5c6ae58cbcbd383351
Thuan Pham <thuanowa@gmail.com>
2023-03-03 23:39:03 +0700

    refactor: bye fish

--- fish/functions/browser_sync_start_server.fish
+++
@@ -1,3 +0,0 @@
-function browser_sync_start_server
-    browser-sync --server --port 9000 --watch --no-open --no-notify --no-ui
-end
--- fish/functions/change_directory_to_git_root.fish
+++
@@ -1,3 +0,0 @@
-function change_directory_to_git_root
-    cd (git rev-parse --show-toplevel)
-end
