8cfbb1ca3f53b0da0316a2cfff1c4bc1a40d007c
Thuan Pham <thuanowa@gmail.com>
2023-03-20 22:50:54 +0700

    refactor(nvim): clean

--- nvim/lua/plugins/disable.lua
+++
@@ -1,6 +0,0 @@
-return {
-  { "rcarriga/nvim-notify",   enabled = false },
-  { "goolord/alpha-nvim",     enabled = false },
-  { "SmiteshP/nvim-navic",    enabled = false },
-  { "folke/persistence.nvim", enable = false },
-}
