cc7ccca559cee5fd9049bbad84df3b4992ed3088
Thuan Pham <thuanowa@gmail.com>
2023-03-21 19:25:08 +0700

    fix(fish): typo

--- fish/functions/fzf_git_worktree_change_dir.fish
+++ fish/functions/fzf_git_worktree_change_dir.fish
@@ -4,8 +4,9 @@ function fzf_git_worktree_change_dir
         cd $worktree
     else
         set -l default_git_folder (git worktree list | head --lines 1 | awk '{print $1}')
-        git worktree add $default_git_folder/.worktree/$argv
-        cd $default_git_folder/.worktree/$argv
+        mkdir $default_git_folder/.worktrees
+        git worktree add $default_git_folder/.worktrees/$argv
+        cd $default_git_folder/.worktrees/$argv
         git switch -c $argv
     end
 
