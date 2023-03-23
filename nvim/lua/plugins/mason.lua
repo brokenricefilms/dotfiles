047d435de17e81b1ea3c8de9a33888e458dd9b49
Thuan Pham <thuanowa@gmail.com>
2023-03-23 14:31:42 +0700

    refactor(nvim): mason auto install lsp

---
+++ nvim/lua/plugins/mason.lua
@@ -0,0 +1,29 @@
+return {
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "astro-language-server",
+      "bash-language-server",
+      "clangd",
+      "css-lsp",
+      "cssmodules-language-server",
+      "deno",
+      "diagnostic-languageserver",
+      "emmet-ls",
+      "flake8",
+      "html-lsp",
+      "json-lsp",
+      "lua-language-server",
+      "prettierd",
+      "shellcheck",
+      "shfmt",
+      "stylua",
+      "svelte-language-server",
+      "tailwindcss-language-server",
+      "typescript-language-server",
+      "unocss-language-server",
+      "vue-language-server",
+      "write-good",
+    }
+  },
+}
