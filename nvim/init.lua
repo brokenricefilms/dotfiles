1605b340d0f290e9185a5e46843660267e2bf11b
Thuan Pham <thuanowa@gmail.com>
2023-03-29 16:06:19 +0700

    ref(nvim): clean

--- nvim/init.lua
+++ nvim/init.lua
@@ -75,13 +75,8 @@ require("lazy").setup({
   },
   {
     "kylechui/nvim-surround",
-    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    version = "*",
     event = "VeryLazy",
-    config = function()
-      require("nvim-surround").setup({
-        -- Configuration here, or leave empty to use defaults
-      })
-    end
   },
 })
 
