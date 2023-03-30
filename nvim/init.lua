30afc556157bfd4af7fb25707ecdbbe75569da14
Thuan Pham <thuanowa@gmail.com>
2023-03-30 20:27:01 +0700

    ref(nvim): clean

--- nvim/init.lua
+++ nvim/init.lua
@@ -120,31 +120,6 @@ require("lazy").setup({
     event = "VeryLazy",
     config = function()
       require("nvim-surround").setup()
-      require("nvim-surround").buffer_setup({
-        surrounds = {
-          ["l"] = {
-            add = function()
-              local clipboard = vim.fn.getreg("+"):gsub("\n", "")
-              return {
-                { "[" },
-                { "](" .. clipboard .. ")" },
-              }
-            end,
-            find = "%b[]%b()",
-            delete = "^(%[)().-(%]%b())()$",
-            change = {
-              target = "^()()%b[]%((.-)()%)$",
-              replacement = function()
-                local clipboard = vim.fn.getreg("+"):gsub("\n", "")
-                return {
-                  { "" },
-                  { clipboard },
-                }
-              end,
-            },
-          },
-        },
-      })
     end,
   },
   {
