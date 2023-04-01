de3fb6588e514db63cb4d15224b2da6e2529cbb3
Thuan Pham <thuanowa@gmail.com>
2023-04-01 14:12:14 +0700

    refactor(nvim): bye pyright lsp

--- nvim/lua/plugins/LSP.lua
+++ nvim/lua/plugins/LSP.lua
@@ -1,23 +1,22 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        html = {},
-        cssls = { settings = { css = { lint = { unknownAtRules = 'ignore', }, }, }, },
-        cssmodules_ls = {},
-        tailwindcss = {},
-        svelte = {},
-        vuels = {},
-        astro = {},
-        denols = {},
-        emmet_ls = {},
-        clangd = {},
-        bashls = {},
-        rust_analyzer = {},
-        gopls = {},
-        pyright = {},
-      },
-    },
-  },
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			servers = {
+				html = {},
+				cssls = { settings = { css = { lint = { unknownAtRules = "ignore" } } } },
+				cssmodules_ls = {},
+				tailwindcss = {},
+				svelte = {},
+				vuels = {},
+				astro = {},
+				denols = {},
+				emmet_ls = {},
+				clangd = {},
+				bashls = {},
+				rust_analyzer = {},
+				gopls = {},
+			},
+		},
+	},
 }
