3f5f07a7fbe2a935c33b9220254731d8a2d8d21b
Thuan Pham <thuanowa@gmail.com>
2023-04-01 14:09:13 +0700

    refactor(nvim): clean

--- nvim/lua/plugins/UI.lua
+++ nvim/lua/plugins/UI.lua
@@ -21,34 +21,4 @@ return {
 			},
 		},
 	},
-	{
-		"nvim-telescope/telescope.nvim",
-		event = "VeryLazy",
-		opts = {
-			defaults = {
-				results_title = false,
-
-				sorting_strategy = "ascending",
-				layout_strategy = "center",
-				layout_config = {
-					preview_cutoff = 1, -- Preview should always show (unless previewer = false)
-
-					width = function(_, max_columns, _)
-						return math.min(max_columns, 80)
-					end,
-
-					height = function(_, _, max_lines)
-						return math.min(max_lines, 15)
-					end,
-				},
-
-				border = true,
-				borderchars = {
-					prompt = { "─", "│", " ", "│", "╭", "╮", "│", "│" },
-					results = { "─", "│", "─", "│", "├", "┤", "╯", "╰" },
-					preview = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
-				},
-			},
-		},
-	},
 }
