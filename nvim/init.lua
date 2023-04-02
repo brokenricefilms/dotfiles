624c5e291a9d6d1af091a01a048494b7c8eb64d6
Thuan Pham <thuanowa@gmail.com>
2023-04-02 12:28:48 +0700

    ref(bash): edir alias hidden file

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,13 +3,7 @@ vim.g.maplocalleader = " "
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
-	})
+	vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable" })
 end
 vim.opt.rtp:prepend(lazypath)
 
