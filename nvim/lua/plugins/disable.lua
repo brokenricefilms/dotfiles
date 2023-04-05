bf6f31f2d73a78fc0adb4fda4bf9ea05460c9d8c
Thuan Pham <thuanowa@gmail.com>
2023-04-05 20:02:53 +0700

    refactor(bash): daily update

--- nvim/lua/plugins/disable.lua
+++ nvim/lua/plugins/disable.lua
@@ -2,7 +2,6 @@ return {
 	{ "goolord/alpha-nvim", enabled = false },
 	{ "folke/tokyonight.nvim", enabled = false },
 	{ "catppuccin/nvim", enabled = false },
-	--{ "echasnovski/mini.comment", enabled = false },
 	{ "ggandor/leap.nvim", enabled = false },
 	{ "ggandor/flit.nvim", enabled = false },
 	{ "folke/persistence.nvim", enabled = false },
