24ad0ed14a145fea41e2665cb0bb36b3dfa43a2a
Thuan Pham <thuanowa@gmail.com>
2023-04-05 22:34:37 +0700

    refactor(neovim): remove neovim config without lsp setup

--- nvim_no_lsp/ftplugin/lua.lua
+++
@@ -1,2 +0,0 @@
-vim.keymap.set("i", ";t", "-- TODO: ", { silent = true, buffer = 0 })
-vim.keymap.set("i", ";f", "function()<enter>end<esc>O", { silent = true, buffer = 0 })
--- nvim_no_lsp/ftplugin/sh.lua
+++
@@ -1,3 +0,0 @@
-vim.keymap.set("i", ";t", "# TODO: ", { silent = true, buffer = 0 })
-vim.keymap.set("i", ";a", 'alias =""<esc>F=i', { silent = true, buffer = 0 })
-vim.keymap.set("i", ";f", "function () {<enter>}<esc>kf(i", { silent = true, buffer = 0 })
