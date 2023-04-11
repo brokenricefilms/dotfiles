4fb57531a4a08dcd0e8504148f3b5704de5973d3
Thuan Pham <thuanowa@gmail.com>
2023-04-11 22:32:23 +0700

    perf(lazyvim): snippet

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -1,6 +1,7 @@
 return {
 	{
 		"rafamadriz/friendly-snippets",
+		event = "InsertEnter",
 		config = function()
 			require("luasnip.loaders.from_snipmate").lazy_load()
 			require("luasnip.loaders.from_vscode").lazy_load()
