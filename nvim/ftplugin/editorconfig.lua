6d0c9f1862e67c52aeac8535572777d6d9d44fda
Thuan Pham <thuanowa@gmail.com>
2023-04-16 10:26:28 +0700

    🎨refactor: naming

--- nvim/ftplugin/editorconfig.lua
+++ nvim/ftplugin/editorconfig.lua
@@ -1,1 +1,1 @@
-vim.keymap.set("i", ";e", "<esc>:read ~/dotfiles/template/.editorconfig<enter>kdd", { silent = true, buffer = 0 })
+vim.keymap.set("i", ";e", "<esc>:read ~/dotfiles/template/skeleton.editorconfig<enter>kdd", { silent = true, buffer = 0 })
