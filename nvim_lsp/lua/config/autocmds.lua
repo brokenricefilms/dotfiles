a66cdc39fc325f37e543d076b5ddf73ea487dbb6
Thuan Pham <thuanowa@gmail.com>
2023-04-17 09:15:54 +0700

    refactor(nvim): autocmds go top in git commit file

--- nvim_lsp/lua/config/autocmds.lua
+++ nvim_lsp/lua/config/autocmds.lua
@@ -3,21 +3,28 @@
 -- Add any additional autocmds here
 
 vim.api.nvim_create_autocmd({ "InsertLeave", "WinEnter" }, {
-	callback = function()
-		local ok, cl = pcall(vim.api.nvim_win_get_var, 0, "auto-cursorline")
-		if ok and cl then
-			vim.wo.cursorline = true
-			vim.api.nvim_win_del_var(0, "auto-cursorline")
-		end
-	end,
+  callback = function()
+    local ok, cl = pcall(vim.api.nvim_win_get_var, 0, "auto-cursorline")
+    if ok and cl then
+      vim.wo.cursorline = true
+      vim.api.nvim_win_del_var(0, "auto-cursorline")
+    end
+  end,
 })
 
 vim.api.nvim_create_autocmd({ "InsertEnter", "WinLeave" }, {
-	callback = function()
-		local cl = vim.wo.cursorline
-		if cl then
-			vim.api.nvim_win_set_var(0, "auto-cursorline", cl)
-			vim.wo.cursorline = false
-		end
-	end,
+  callback = function()
+    local cl = vim.wo.cursorline
+    if cl then
+      vim.api.nvim_win_set_var(0, "auto-cursorline", cl)
+      vim.wo.cursorline = false
+    end
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "gitcommit",
+  callback = function()
+    vim.cmd("normal gg")
+  end,
 })
