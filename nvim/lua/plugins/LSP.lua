19ca42350e0ac14d18435a860a4d8fd8339da3f0
Thuan Pham <thuanowa@gmail.com>
2023-04-29 14:46:23 +0700

    refactor(nvim): no java lsp

--- nvim/lua/plugins/LSP.lua
+++ nvim/lua/plugins/LSP.lua
@@ -26,7 +26,6 @@ return {
         awk_ls = {},
         ansiblels = {},
         eslint = {},
-        java_language_server = {},
         phpactor = {},
         jsonls = {},
         marksman = {},
@@ -40,12 +39,7 @@ return {
     opts = function()
       local nls = require("null-ls")
       return {
-        root_dir = require("null-ls.utils").root_pattern(
-          ".null-ls-root",
-          ".neoconf.json",
-          "Makefile",
-          ".git"
-        ),
+        root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
         sources = {
           nls.builtins.formatting.fish_indent,
           nls.builtins.diagnostics.fish,
