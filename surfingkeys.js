d0fb882fa4226aade3edb392e1b6296d515e1962
Thuan Pham <thuanowa@gmail.com>
2023-05-28 08:58:35 +0700

    feat: pre-commit

--- surfingkeys.js
+++ surfingkeys.js
@@ -5,26 +5,18 @@ api.removeSearchAlias("b");
 api.removeSearchAlias("w");
 api.removeSearchAlias("s");
 
-api.addSearchAlias(
-  "t",
-  "translate",
-  "https://translate.google.com/?sl=en&tl=vi&text=",
-  "s",
-  "https://translate.google.com/?sl=en&tl=vi&text=",
-  function(response) {
-    const res = JSON.parse(response.text);
-    return res.map(function(r) {
-      return r.phrase;
-    });
-  }
-);
+api.addSearchAlias("t", "translate", "https://translate.google.com/?sl=en&tl=vi&text=", "s", "https://translate.google.com/?sl=en&tl=vi&text=", function (response) {
+  const res = JSON.parse(response.text);
+  return res.map(function (r) {
+    return r.phrase;
+  });
+});
 
 api.iunmap(":");
 
 api.Hints.setCharacters("asdghklqwertyuiopzxcvbnmfj/.,;");
 
-const hintsCss =
-  "font-size: 13pt; font-family: 'JetBrains Mono NL', 'Cascadia Code', 'Helvetica Neue', Helvetica, Arial, sans-serif; border: 0px; color: #000000 !important; background: #ffffff; background-color: #ffffff";
+const hintsCss = "font-size: 13pt; font-family: 'JetBrains Mono NL', 'Cascadia Code', 'Helvetica Neue', Helvetica, Arial, sans-serif; border: 0px; color: #000000 !important; background: #ffffff; background-color: #ffffff";
 
 api.Hints.style(hintsCss);
 api.Hints.style(hintsCss, "text");
@@ -34,4 +26,4 @@ settings.theme = `
   font-size: 1rem;
   font-family: 'JetBrains Mono NL', 'Cascadia Code', 'Helvetica Neue', Helvetica, Arial, sans-serif;
 }
-`
+`;
