399d1289f080ea1c9131eddf982f3571abbac201
Thuan Pham <thuanowa@gmail.com>
2023-07-04 20:29:16 +0700

    refactor(nvim): flash extra plugin

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -15,7 +15,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
-    { import = "lazyvim.plugins.extras.editor.flash" },
     { import = "plugins" },
   },
   defaults = {
