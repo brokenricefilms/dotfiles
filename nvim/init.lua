84319837959a2dc05ca0c783568a33725fc83457
Thuan Pham <ongeditvideo@gmail.com>
2023-11-15 17:26:10 +0700

    fix(nvim): keymap

--- nvim/init.lua
+++ nvim/init.lua
@@ -54,7 +54,6 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>f', builtin.find_files, {})
 vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>b', builtin.buffers, {})
-vim.keymap.set('n', '<leader>h', builtin.help_tags, {})
 
 vim.opt.background = "light"
 vim.cmd("colorscheme github_light")
