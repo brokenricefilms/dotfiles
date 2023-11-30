06f627d9f70035fa967329bdf39b01c2be271ab5
Thuan Pham <licitfree@gmail.com>
2023-11-30 10:50:15 +0700

    feat: windows install script

---
+++ install.ps1
@@ -0,0 +1,7 @@
+winget install --id=yt-dlp.yt-dlp  -e
+winget install --id=sharkdp.fd  -e
+winget install --id=mbuilov.sed  -e
+winget install --id=junegunn.fzf -e
+winget install --id=BurntSushi.ripgrep.MSVC -e 
+winget install --id=Git.Git  -e
+winget install --id=GitHub.cli  -e
