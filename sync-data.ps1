81cf9c137a63af589a91290bdb63d654d090baf4
Broken Rice Films <brokenricefilms@gmail.com>
2024-06-11 06:18:02 +0700

    Auto commit

--- sync-data.ps1
+++ sync-data.ps1
@@ -14,5 +14,13 @@ auto-git-commit
 cd C:\Users\master\repos\brokenricefilms\notes
 auto-git-commit
 
+cp $profile ~\repos\brokenricefilms\dotfiles\
+cp C:\Users\master\AppData\Local\nvim\init.lua ~\repos\brokenricefilms\dotfiles\nvim
+cp C:\Users\master\.gitconfig ~\repos\brokenricefilms\dotfiles\
+cp C:\Users\master\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json ~\repos\brokenricefilms\dotfiles\windows-terminal\
+
+whoami.exe >> C:\Users\master\repos\brokenricefilms\dotfiles\test.log
+date >> C:\Users\master\repos\brokenricefilms\dotfiles\test.log
+
 cd C:\Users\master\repos\brokenricefilms\dotfiles
 auto-git-commit
